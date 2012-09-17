bdf7c58aa1495db5a82d82dd692795f8cca1a28f
Andy Gooding <andy@citrusleaf.net>
2012-09-17 10:25:26 -0700

    Removed stray experimental code snippet.

--- cf_base/include/citrusleaf/cf_log.h
+++ cf_base/include/citrusleaf/cf_log.h
@@ -5,8 +5,6 @@
 
 #include <stdarg.h>
 
-void cf_error(const char* fmt, ...);
-
 typedef enum {
 	CF_NO_LOGGING = -1,
 	CF_ERROR,
@@ -32,7 +30,14 @@ static inline void cf_set_log_callback(cf_log_callback callback)
 	}
 }
 
-#define cf_error(__fmt, __args...) if (CF_ERROR <= g_log_level) {(*g_log_callback)(CF_ERROR, __fmt, ## __args);}
-#define cf_warn(__fmt, __args...) if (CF_WARN <= g_log_level) {(*g_log_callback)(CF_WARN, __fmt, ## __args);}
-#define cf_info(__fmt, __args...) if (CF_INFO <= g_log_level) {(*g_log_callback)(CF_INFO, __fmt, ## __args);}
-#define cf_debug(__fmt, __args...) if (CF_DEBUG <= g_log_level) {(*g_log_callback)(CF_DEBUG, __fmt, ## __args);}
+#define cf_error(__fmt, __args...) \
+	if (CF_ERROR <= g_log_level) {(*g_log_callback)(CF_ERROR, __fmt, ## __args);}
+
+#define cf_warn(__fmt, __args...) \
+	if (CF_WARN <= g_log_level) {(*g_log_callback)(CF_WARN, __fmt, ## __args);}
+
+#define cf_info(__fmt, __args...) \
+	if (CF_INFO <= g_log_level) {(*g_log_callback)(CF_INFO, __fmt, ## __args);}
+
+#define cf_debug(__fmt, __args...) \
+	if (CF_DEBUG <= g_log_level) {(*g_log_callback)(CF_DEBUG, __fmt, ## __args);}

741ee4dc17ea318ec44c6006ed2bf29175dfde65
Wei-Ling Chu <weiling@citrusleaf.com>
2013-01-11 19:36:17 -0800

    putting in correct version of ascli.

--- cl_c/src/test/ascli.py
+++ cl_c/src/test/ascli.py
@@ -1,28 +1,45 @@
 #!/usr/bin/python
 import sys
 import getopt
+from subprocess import call
 
-def log(msg):
-    fptr.write( "%s, %s\n" %(datetime.now().strftime("%Y-%m-%d %H:%M:%S"),msg))
-    fptr.flush()    
+def Usage():
+	print ('Helper script to Execute a command against an Aerospike cluster')
+	print ('Usage: ascli')
+	print ('-l, --location           location of the executables')
+	print ('     	                 default: /opt/citrusleaf/bin/commands')
+	print ('-u, --usage              usage')
+	print ('-v, --verbose            verbose')
+	print ('')
+	print ('Example: ')
+	print ('ascli udf-record-apply test myset mykey test sum 4 5')
+	sys.exit(0)
 
 if __name__ == '__main__':
 
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "p:h:o:v")
-    except getopt.GetoptError, err:
-        log (str(err))
-        usage()
-        sys.exit(-1)
-	print opts
-	print args
-    for o, a in opts:
-        if ((o == "-p") or (o == "--port")):
-            port = int(a)
-        if ((o == "-h") or (o == "--host")):
-            host = a
-        if ((o == "-o") or (o == "--operand")):
-            operand = a
-#		if (operand not in ("exists", "get", "put", "quick", "remove", "udf", "udf-get", "udf-list", "udf-put", "udf-record-apply","udf-remove")):
-#			raise Usage()
+	loc = "/opt/citrusleaf/bin/commands"
+	verbose = False
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "l:uv")
+	except getopt.GetoptError, err:
+		print (str(err))
+		Usage()
+		sys.exit(-1)
+	for o, a in opts:
+		if ((o == "-l") or (o == "--location")):
+			loc = a
+		if ((o == "-u") or (o == "--usage")):
+			Usage()
+		if ((o == "-v") or (o == "--verbose")):
+			verbose = True;
+
+	if (len(args)<1):
+		Usage()
+		sys.exit(-2)		
+
+	args[0] = loc+"/"+args[0]
+	if verbose:
+		print 'executing: ' + ' '.join(args)
+
+	call(args)
 

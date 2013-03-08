31924b2eeadc4f0f8816816064b1638e57b35057
Toby Lehman <toby@aerospike.com>
2013-03-07 19:37:17 -0800

    Modification of Compress5

--- cl_c/udf_examples/lua_files/UdfFunctionTable.lua
+++ cl_c/udf_examples/lua_files/UdfFunctionTable.lua
@@ -279,17 +279,17 @@ function UdfFunctionTable.stumbleUnCompress5( b18, arglist )
 
   local stumbleTuple = list(5);
   -- NOTE: Must append.  Can't index directly into it.
-  list.append( stumbleTuple, bytes.get_int32(b18, 1 ));  -- 4 byte int
-  list.append( stumbleTuple, bytes.get_int32(b18, 5 ));  -- 4 byte int
-  list.append( stumbleTuple, bytes.get_int32(b18, 9 ));  -- 4 byte int
-  list.append( stumbleTuple, bytes.get_int32(b18, 13)); -- 4 byte int
-  list.append( stumbleTuple, bytes.get_int16(b18, 17));  -- 2 byte int
+--  list.append( stumbleTuple, bytes.get_int32(b18, 1 ));  -- 4 byte int
+--  list.append( stumbleTuple, bytes.get_int32(b18, 5 ));  -- 4 byte int
+--  list.append( stumbleTuple, bytes.get_int32(b18, 9 ));  -- 4 byte int
+--  list.append( stumbleTuple, bytes.get_int32(b18, 13)); -- 4 byte int
+--  list.append( stumbleTuple, bytes.get_int16(b18, 17));  -- 2 byte int
 
---  stumbleTuple[1] = bytes.get_int32(b18, 1 );  -- 4 byte int
---  stumbleTuple[2] = bytes.get_int32(b18, 5 );  -- 4 byte int
---  stumbleTuple[3] = bytes.get_int32(b18, 9 );  -- 4 byte int
---  stumbleTuple[4] = bytes.get_int32(b18, 13 ); -- 4 byte int
---  stumbleTuple[5] = bytes.get_int16(b18, 17);  -- 2 byte int
+  stumbleTuple[1] = bytes.get_int32(b18, 1 );  -- 4 byte int
+  stumbleTuple[2] = bytes.get_int32(b18, 5 );  -- 4 byte int
+  stumbleTuple[3] = bytes.get_int32(b18, 9 );  -- 4 byte int
+  stumbleTuple[4] = bytes.get_int32(b18, 13 ); -- 4 byte int
+  stumbleTuple[5] = bytes.get_int16(b18, 17);  -- 2 byte int
 
   info("[EXIT]: <%s:%s> TupleResult(%s) type(%s)\n",
     mod, meth, tostring(stumbleTuple), type(stumbleTuple ));

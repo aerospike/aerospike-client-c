80eb0b128f8c001f21d78dff029bd96c2b69877f
Russ Sullivan <russ@citrusleaf.com>
2013-03-09 06:51:59 -0800

    updating Validate_LSET.sh

--- cl_c/udf_examples/as_set_udf/Validate_LSET.sh
+++ cl_c/udf_examples/as_set_udf/Validate_LSET.sh
@@ -4,7 +4,7 @@ export UNS="test"
 export USET="set"
 
 export SETBIN="SetBin"
-export UFILE="AsLSetStrawman"
+export UFILE="AsLSetStickman"
 export FUN_CR8="asLSetCreate"
 export FUN_INS="asLSetInsert"
 export FUN_SRCH="asLSetSearch"
@@ -14,12 +14,14 @@ VAL=1
 USERID=300
 
 SIZES="10 20 50 100 150 200 250 300 400 500 600 700 800 1000 2000 10000"
+#SIZES="10 20"
 
 for NUM_PUSHES in $SIZES; do
+  ascli udf-record-apply $UNS $USET $USERID $UFILE $FUN_CR8 $UNS $USET $SETBIN 0
   RAN=$(lua -e "math.randomseed(os.time());print(math.random(77))");
   echo "Pushing: NUM_PUSHES: $NUM_PUSHES RAN: $RAN"
   I=1; while [ $I -le $NUM_PUSHES ]; do
-    ascli udf-record-apply $UNS $USET $USERID $UFILE $FUN_INS $SETBIN $VAL > /dev/null
+    ascli udf-record-apply $UNS $USET $USERID $UFILE $FUN_INS $UNS $USET $SETBIN 0 $VAL > /dev/null
     I=$[${I}+1];
     VAL=$[${VAL}+${RAN}];
   done

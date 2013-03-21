76dccb77c3aca895d9da4357858fe496f8ac6a94
Sunanda Sharma <sunanda@aerospike.com>
2013-03-21 03:45:07 -0400

    Running parallel scans

---
+++ cl_c/udf_examples/scan_udf/run_parallel_scans.sh
@@ -0,0 +1,6 @@
+#Run five scan instances with 10 threads each
+./scan_udf -n "test" -t 10 &
+./scan_udf -n "test" -t 10 &
+./scan_udf -n "test" -t 10 &
+./scan_udf -n "test" -t 10 &
+./scan_udf -n "test" -t 10 &

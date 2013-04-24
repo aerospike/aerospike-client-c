5b29558adabab8299acc3a443101281b4d707793
Chris Stivers <chris@stivers.us>
2013-04-24 15:43:41 -0700

    removed script

--- aerospike/modules/update_mod_lua_to_current_master.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -rf mod-lua
-git clone git@github.com:aerospike/aerospike-mod-lua.git
-mv aerospike-mod-lua mod-lua
-git add mod-lua
-(cd mod-lua; git submodule init; git submodule update)

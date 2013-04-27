86aabeebc332001bd3a9a756db85a0036f46c1bf
Toby Lehman <toby@aerospike.com>
2013-04-26 17:48:16 -0700

    Growing the ATF LSTACK tests

--- aerospike/src/test/lstack/lstack_basics.c
+++ aerospike/src/test/lstack/lstack_basics.c
@@ -1,120 +1,67 @@
 
-#include "../test.h"
-#include "../util/udf.h"
-#include <citrusleaf/citrusleaf.h>
-#include <citrusleaf/as_types.h>
-#include <citrusleaf/aerospike_lstack.h>
+// #include "../test.h"
+// #include "../util/udf.h"
+// #include <citrusleaf/citrusleaf.h>
+// #include <citrusleaf/as_types.h>
+// #include <citrusleaf/aerospike_lstack.h>
 
-/******************************************************************************
- * UTILITY FUNCTION
- *****************************************************************************/
-void __log_append(FILE * f, const char * prefix, const char * fmt, ...) {
-    char msg[128] = {0};
-    va_list ap;
-    va_start(ap, fmt);
-    vsnprintf(msg, 128, fmt, ap);
-    va_end(ap);
-    fprintf(f, "%s%s\n",prefix,msg);
-}
+#include "lstack_test.h"
 
+// LSTACK BASICS: 
+// In the LSTACK ATF, define the tests for the very basic operations:
+// (*) basic push()
+// (*) basic peek()
+// (*) basic size()
+// (*) basic config()
+
+static char * MOD = "lstack_basics.c::13_04_26";
 /******************************************************************************
  * TEST CASES
  *****************************************************************************/
-static int lso_push_quintuplet(char * ns, char * set, char * key,
-                               char * lso_bin_name, int val) {
-    extern cl_cluster * cluster;
-
-    cl_write_parameters wp;
-    cl_write_parameters_set_default(&wp);
-    wp.timeout_ms = 1000;
-    wp.record_ttl = 864000;
-
-    // Set the Create Spec to use the StumbleUpon Package.
-    // char * compress_func   = "stumbleCompress5";
-    as_map *create_args = as_hashmap_new( 2, 2 );
-    as_map_set( create_args,
-            (as_val *) as_string_new("Package", false ),
-            (as_val *) as_string_new("StumbleUpon", false ));
-
-
-    as_list * listp = as_arraylist_new( 5, 5 );
-    int64_t urlid   = val + 1;
-    as_list_add_integer( listp, urlid );
-    int64_t created = val + 2;
-    as_list_add_integer( listp, created );
-    int64_t meth_a  = val + 3;
-    as_list_add_integer( listp, meth_a );
-    int64_t meth_b  = val + 4;
-    as_list_add_integer( listp, meth_b );
-    int64_t status  = val + 5;
-    as_list_add_integer( listp, status );
-
-    return aerospike_lstack_push_with_create(
-            cluster, ns, set, key, lso_bin_name, (as_val *)listp,
-            create_args, wp.timeout_ms);
-    as_list_destroy(listp);
-    as_list_destroy(create_args);
-}
-
+// int lstack_push_test(char * keystr, char * ldt_bin, int iterations,
+//                       int seed, int data_format )
+// int lstack_peek_test(char * keystr, char * ldt_bin, int iterations,
+//                       int seed, int data_format )
 // ==================================================================
-TEST( aerospike_lstack_push_1, "aerospike_lstack_push_with_create" ) {
-    char * user_key     = "User_111";
-    char * lso_bin_name = "number_stack";
+TEST( lstack_basics_push, "Push N items on the stack" ) {
+    char * user_key   = "User_111";
+    char * ldt_bin    = "number_stack";
+    int    iterations = 10;
+    int    seed       = 111;
+    int    format     = NUMBER_FORMAT;
 
-    int rc = lso_push_quintuplet("test", "unit", user_key, lso_bin_name, 5);
+    int rc = lstack_push_test( user_key, ldt_bin, iterations, seed, format );
     assert_int_eq( rc, 0 );
-}
+} // end TEST
 
 // ==================================================================
-static int lso_peek_quintuplet(char * ns, char * set,
-                               char * key, char * lso_bin_name) {
-    extern cl_cluster * cluster;
+TEST( lstack_basics_peek, "Read N elements from the stack" ) {
+    char * user_key   = "User_111";
+    char * ldt_bin    = "number_stack";
+    int    iterations = 10;
+    int    seed       = 111;
+    int    format     = NUMBER_FORMAT;
 
-    cl_write_parameters wp;
-    cl_write_parameters_set_default(&wp);
-    wp.timeout_ms = 1000;
-    wp.record_ttl = 864000;
-
-    // char * uncompress_func = "stumbleUnCompress5";
-    // as_list *uncompress_args = as_arraylist_new( 1, 1 );
-    // as_list_add_integer( uncompress_args, 1 ); // dummy argument
-
-    int peek_count = 1;
-    as_result * resultp = aerospike_lstack_peek(
-            cluster, ns, set, key, lso_bin_name, peek_count, wp.timeout_ms);
-    if ( resultp ) {
-        int issuccess = resultp->is_success;
-        return resultp->is_success;
-    }
-    else {
-        return false;
-    }
-}
-
-TEST( aerospike_lstack_peek_1, "aerospike_lstack_peek" ) {
-    char * user_key     = "User_111";
-    char * lso_bin_name = "number_stack";
-
-    int rc = lso_peek_quintuplet("test", "unit", user_key, lso_bin_name);
+    int rc = lstack_peek_test( user_key, ldt_bin, iterations, seed, format );
     assert_int_eq( rc, 1 );
 }
 
-/******************************************************************************
- * TEST SUITE
- *****************************************************************************/
+// ==================================================================
+TEST( lstack_basics_size, "validate the stack size" ) {
+    char * user_key     = "User_111";
+    char * ldt_bin_name = "number_stack";
+    int rc = 1;
 
-static bool before(atf_suite * suite) {
-    return true;
+    assert_int_eq( rc, 1 );
 }
 
-static bool after(atf_suite * suite) {
-    return true;
+// ==================================================================
+TEST( lstack_basics_config, "Validate Config Settings" ) {
+    char * user_key     = "User_111";
+    char * ldt_bin_name = "number_stack";
+    int rc = 1;
+
+    assert_int_eq( rc, 1 );
 }
 
-SUITE( lstack_basics, "test basics.lua" ) {
-    suite_before( before );
-    suite_after( after );
-
-    suite_add( aerospike_lstack_push_1 );
-    suite_add( aerospike_lstack_peek_1 );
-}
+// ==================================================================
---
+++ aerospike/src/test/lstack/lstack_test.c
@@ -0,0 +1,253 @@
+// #include "../test.h"
+// #include "../util/udf.h"
+// #include <citrusleaf/citrusleaf.h>
+// #include <citrusleaf/as_types.h>
+// #include <citrusleaf/aerospike_lstack.h>
+
+#include "lstack_test.h"  // Everything a growing test needs
+
+// LSTACK TESTS: ATF Overview
+// In the global Aerospike Test Framework (ATF), there is a mechanism to
+// automate tests for each Aerospike feature.  At the high level, there is
+// one (or potentially multiple) Test Plans.
+// A Test Plan comprises multiple Test Suite.  A Suite will usually correspond
+// to a feature (like LSTACK).  In a Test Suite will be multiple TESTS.
+//
+// The General structure is this:
+// A Suite defines the "before test" function, the individual test functions
+// and the "after test" function.
+// Each Test is expected to exercise some specific function of the feature
+// (i.e. the Suite).  It will end with an assertion that the output of the
+// system matches the expected output.
+// Version 1: We down our own tests on output (awkward and ugly).
+// Version 2: We capture the output into a file -- and then invoke the output
+// compare function to tell us if the files match (easier).
+
+static char * MOD = "lstack_test.c::13_04_26";
+
+/******************************************************************************
+ * TESTS
+ *****************************************************************************/
+
+// ++====================++
+// || BASIC   OPERATIONS ||
+// ++====================++
+
+TEST( lstack_basics_push, "Push N items on the stack" ) {
+    char * user_key   = "User_111";
+    char * ldt_bin    = "number_stack";
+    int    iterations = 10;
+    int    seed       = 111;
+    int    format     = NUMBER_FORMAT;
+
+    int rc = lstack_push_test( user_key, ldt_bin, iterations, seed, format );
+    assert_int_eq( rc, 0 );
+} // end TEST
+
+TEST( lstack_basics_peek, "Read N elements from the stack" ) {
+    char * user_key   = "User_111";
+    char * ldt_bin    = "number_stack";
+    int    iterations = 10;
+    int    seed       = 111;
+    int    format     = NUMBER_FORMAT;
+
+    int rc = lstack_peek_test( user_key, ldt_bin, iterations, seed, format );
+    assert_int_eq( rc, 1 );
+}
+
+TEST( lstack_basics_size, "validate the stack size" ) {
+    char * user_key     = "User_111";
+    char * ldt_bin_name = "number_stack";
+    int rc = 1;
+
+    assert_int_eq( rc, 1 );
+}
+
+TEST( lstack_basics_config, "Validate Config Settings" ) {
+    char * user_key     = "User_111";
+    char * ldt_bin_name = "number_stack";
+    int rc = 1;
+
+    assert_int_eq( rc, 1 );
+}
+
+
+// ++====================++
+// || REGULAR OPERATIONS ||
+// ++====================++
+
+TEST( lstack_operations_small_push, "lstack push small" ) {
+    static char * meth = "lstack_operations_small_push()";
+    int rc = 0;
+    printf("Test(%s) called\n", meth );
+
+    // TODO: Fill in Function
+    
+    assert_int_eq( rc, 0 );
+} // end lstack_operations_small_push()
+
+TEST( lstack_operations_medium_push, "lstack push medium" ) {
+    static char * meth = "lstack_operations_medium_push()";
+    int rc = 0;
+    printf("Test(%s) called\n", meth );
+
+    // TODO: Fill in Function
+    
+    assert_int_eq( rc, 0 );
+} // end   lstack_operations_medium_push()
+
+TEST( lstack_operations_large_push, "lstack push large" ) {
+    static char * meth = "lstack_operations_large_push()";
+    int rc = 0;
+    printf("Test(%s) called\n", meth );
+
+    // TODO: Fill in Function
+    
+    assert_int_eq( rc, 0 );
+} // end   lstack_operations_large_push()
+
+
+TEST( lstack_operations_small_peek, "lstack peek small" ) {
+    static char * meth = "lstack_operations_small_peek()";
+    int rc = 0;
+    printf("Test(%s) called\n", meth );
+
+    // TODO: Fill in Function
+    
+    assert_int_eq( rc, 0 );
+} // end   lstack_operations_small_peek()
+
+TEST( lstack_operations_medium_peek, "lstack peek medium" ) {
+    static char * meth = "lstack_operations_medium_peek()";
+    int rc = 0;
+    printf("Test(%s) called\n", meth );
+
+    // TODO: Fill in Function
+    
+    assert_int_eq( rc, 0 );
+} // end   lstack_operations_medium_peek()
+
+TEST( lstack_operations_large_peek, "lstack peek large" ) {
+    static char * meth = "lstack_operations_large_peek()";
+    int rc = 0;
+    printf("Test(%s) called\n", meth );
+
+    // TODO: Fill in Function
+    
+    assert_int_eq( rc, 0 );
+} // end   lstack_operations_large_peek()
+
+// ++=====================++
+// || ADVANCED OPERATIONS ||
+// ++=====================++
+
+TEST( lstack_advanced_push_compact, "lstack Adv push compact" ) {
+    static char * meth = "lstack_advanced_push_compact";
+    int rc = 0;
+    printf("Test(%s) called\n", meth );
+
+    // TODO: Fill in Function
+    
+    assert_int_eq( rc, 0 );
+} // end lstack_advanced_push_compact
+
+TEST( lstack_advanced_push_objects, "lstack Adv push objects" ) {
+    static char * meth = "lstack_advanced_push_objects()";
+    int rc = 0;
+    printf("Test(%s) called\n", meth );
+
+    // TODO: Fill in Function
+    
+    assert_int_eq( rc, 0 );
+} // end   lstack_advanced_push_objects()
+
+TEST( lstack_advanced_push_objects_compact, "lstack adv push Obj Compact" ) {
+    static char * meth = "lstack_advanced_push_objects_compact()";
+    int rc = 0;
+    printf("Test(%s) called\n", meth );
+
+    // TODO: Fill in Function
+    
+    assert_int_eq( rc, 0 );
+} // end   lstack_advanced_push_objects_compact()
+
+
+TEST( lstack_advanced_peek_compact, "lstack Adv peek compact" ) {
+    static char * meth = "lstack_advanced_peek_compact()";
+    int rc = 0;
+    printf("Test(%s) called\n", meth );
+
+    // TODO: Fill in Function
+    
+    assert_int_eq( rc, 0 );
+} // end   lstack_advanced_peek_compact()
+
+TEST( lstack_advanced_peek_objects, "lstack Adv peek obj" ) {
+    static char * meth = "lstack_advanced_peek_objects()";
+    int rc = 0;
+    printf("Test(%s) called\n", meth );
+
+    // TODO: Fill in Function
+    
+    assert_int_eq( rc, 0 );
+} // end   lstack_advanced_peek_objects()
+
+TEST( lstack_advanced_peek_objects_and_filter, "lstack Adv peek Obj & Filter" ) {
+    static char * meth = "lstack_advanced_peek_objects_and_filter()";
+    int rc = 0;
+    printf("Test(%s) called\n", meth );
+
+    // TODO: Fill in Function
+    
+    assert_int_eq( rc, 0 );
+} // end   lstack_advanced_peek_objects_and_filter()
+
+TEST( lstack_advanced_peek_objects_compact_and_filter,
+        "lstack Adv peek Obj Compact & Filter" )
+{
+    static char * meth = "lstack_advanced_peek_objects_compact_and_filter()";
+    int rc = 0;
+    printf("Test(%s) called\n", meth );
+
+    // TODO: Fill in Function
+    
+    assert_int_eq( rc, 0 );
+} // end   lstack_advanced_peek_objects_compact_and_filter()
+
+
+/******************************************************************************
+ * TEST SUITE
+ *****************************************************************************/
+
+static bool lstack_before(atf_suite * suite) {
+    lstack_setup();
+    return true;
+}
+
+static bool lstack_after(atf_suite * suite) {
+    shutdown_test();
+    return true;
+}
+
+SUITE( lstack_basics, "LSTACK.lua" ) {
+    suite_before( lstack_before );
+    suite_after( lstack_after );
+
+    suite_add( lstack_basics_push );
+    suite_add( lstack_basics_peek );
+    suite_add( lstack_basics_size );
+    suite_add( lstack_basics_config );
+
+    suite_add( lstack_operations_small_push );
+    suite_add( lstack_operations_small_peek );
+    suite_add( lstack_operations_medium_push );
+    suite_add( lstack_operations_medium_peek );
+    suite_add( lstack_operations_large_peek );
+    suite_add( lstack_operations_large_push );
+
+    suite_add( lstack_advanced_push_compact );
+    suite_add( lstack_advanced_peek_compact );
+    suite_add( lstack_advanced_push_objects );
+    suite_add( lstack_advanced_peek_objects );
+    suite_add( lstack_advanced_peek_objects_and_filter );
+}

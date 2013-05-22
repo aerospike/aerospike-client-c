efd7f3c004cf409372a6ce87268609f7a07dbc93
Sunanda Sharma <sunanda@aerospike.com>
2013-05-22 04:48:24 -0400

    [AER-679] Nested collection

--- aerospike/src/test/lua/client_record_basics.lua
+++ aerospike/src/test/lua/client_record_basics.lua
@@ -60,3 +60,61 @@ function delete(record)
 	record.bin3 = nil
 	return aerospike:update(record)
 end
+
+function return_types(record, desired_type)
+
+  if (desired_type == "none") then
+    print("none");
+    return nil;
+  end
+  if (desired_type == "p_int_primitive") then
+    print("positive int");
+    return 5;
+  end  
+  if (desired_type == "n_int_primitive") then
+    print("negative int");
+    return -5;
+  end  
+  if (desired_type == "string_primitive") then
+    print("string");
+    return "good";
+  end  
+  if (desired_type == "bin_array") then
+    print("bin_array");
+    local l = list();
+    list.append(l,'have s1');
+    list.append(l,55);
+    return  l;
+  end
+  if (desired_type == "bin_nested_list") then
+    info("bin_nested_list");
+	  local x = list();
+    local y = list();
+    list.append(x,1);
+    list.append(x,'yup');
+    list.append(y,'string_resp');
+    list.append(y,x);
+    return y
+  end
+  if (desired_type == "bin_map") then
+    info("bin_map");
+    
+    local m = map{};
+    m["i"] = 456
+    m["s"] = "def"
+    m["l"] = list{4,5,6};
+
+    local l = list();
+    list.append(l,456);
+    list.append(l,"def");
+
+    local x = map{};
+    x["i"] = 123;
+    x["s"] = "abc";
+    x["l"] = l;
+    x["m"] = m;
+    return x;
+  end
+end
+
+

a91f04c1540ddfcd6bfee5a75383e1ffaa8d7f22
Bhuvana Ramkumar <bhuvana@aerospike.com>
2013-06-13 11:40:03 -0700

    string and list for key_apply2.c

--- aerospike/src/test/lua/key_apply2.lua
+++ aerospike/src/test/lua/key_apply2.lua
@@ -7,3 +7,24 @@ function file_exists(file, a)
         return 0
     end
 end
+
+function getboolean(record)
+    return true
+end
+
+function getinteger(record)
+    return 123
+end
+
+function getstring(record)
+    return "abc"
+end
+
+function getlist(record)
+	as_list list;
+	as_arraylist_init(&list, 3, 0);
+	as_list_append_int64(&list, 1);
+	as_list_append_int64(&list, 2);
+	as_list_append_int64(&list, 3);
+    return list
+end

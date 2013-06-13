8f41ee33500b1f9c4a5985dec02f67f99768b56c
jyoti <jyoti@localhost.localdomain>
2013-06-13 12:52:59 +0530

    test change from sindex to index

--- aerospike/src/test/aerospike_index/index_basics.c
+++ aerospike/src/test/aerospike_index/index_basics.c
@@ -1,6 +1,6 @@
 
 #include <aerospike/aerospike.h>
-#include <aerospike/aerospike_scan.h>
+#include <aerospike/aerospike_index.h>
 
 #include <aerospike/as_error.h>
 #include <aerospike/as_status.h>
@@ -36,14 +36,38 @@ extern aerospike * as;
  * TEST CASES
  *****************************************************************************/
 
-TEST( index_basics_1 , "basics 1" ) {
-	assert_not_null(NULL);
+TEST( index_basics_create , "Create index on bin" ) {
+
+    as_error err;
+    as_error_reset(&err);
+
+    aerospike_index_sparse_create(as, &err, NULL, "test", "test", "new_bin", AS_TYPE_STR, "idx_test_new_bin");
+    if ( err.code != AEROSPIKE_OK && err.code != AEROSPIKE_ERR_INDEX_EXISTS ) {
+        info("error(%d): %s", err.code, err.message);
+    }
+    assert_int_eq( err.code , AEROSPIKE_OK );
+
 }
+
+TEST( index_basics_drop , "Drop index" ) {
+
+    as_error err;
+    as_error_reset(&err);
+
+    aerospike_index_remove(as, &err, NULL, "test", "idx_test_new_bin");
+    if ( err.code != AEROSPIKE_OK ) {
+        info("error(%d): %s", err.code, err.message);
+    }
+    assert_int_eq( err.code, AEROSPIKE_OK );
+
+}
+
 
 /******************************************************************************
  * TEST SUITE
  *****************************************************************************/
 
 SUITE( index_basics, "aerospike_sindex basic tests" ) {
-	suite_add( index_basics_1 );
+    suite_add( index_basics_create );
+    suite_add( index_basics_drop );
 }

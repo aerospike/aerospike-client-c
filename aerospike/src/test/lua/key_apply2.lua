5cc6a513b19407f28bbac01b23e57a4d3dd60fde
Bhuvana Ramkumar <bhuvana@aerospike.com>
2013-06-17 19:16:01 -0700

    record api's ported to new client api

--- aerospike/src/test/lua/key_apply2.lua
+++ aerospike/src/test/lua/key_apply2.lua
@@ -22,3 +22,15 @@ end
 function add_strings(rec, a, b)
     return a + b
 end
+
+function sum(record, a, b)
+    return add(record, a, b)
+end
+
+function add_local(rec, a, b)
+    return a + b
+end
+
+function sum_local(record, a, b)
+    return add_local(record, a, b)
+end

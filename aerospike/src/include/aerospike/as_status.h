d501107223aeef13ac1cec27082470a54485ab52
Andrew Gooding <andy@aerospike.com>
2013-06-20 13:58:27 -0700

    Filled out as_status and the shim to convert cl error codes to as_status. Work in progress.

--- aerospike/src/include/aerospike/as_status.h
+++ aerospike/src/include/aerospike/as_status.h
@@ -1,109 +1,277 @@
-/******************************************************************************
+/*******************************************************************************
  *	Copyright 2008-2013 by Aerospike.
  *
- *	Permission is hereby granted, free of charge, to any person obtaining a copy 
- *	of this software and associated documentation files (the "Software"), to 
- *	deal in the Software without restriction, including without limitation the 
- *	rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
- *	sell copies of the Software, and to permit persons to whom the Software is 
+ *	Permission is hereby granted, free of charge, to any person obtaining a copy
+ *	of this software and associated documentation files (the "Software"), to
+ *	deal in the Software without restriction, including without limitation the
+ *	rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ *	sell copies of the Software, and to permit persons to whom the Software is
  *	furnished to do so, subject to the following conditions:
- *	
- *	The above copyright notice and this permission notice shall be included in 
+ *
+ *	The above copyright notice and this permission notice shall be included in
  *	all copies or substantial portions of the Software.
- *	
+ *
  *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  *	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  *	IN THE SOFTWARE.
- *****************************************************************************/
+ ******************************************************************************/
 
-#pragma once 
+#pragma once
 
-/******************************************************************************
+/*******************************************************************************
  *	TYPES
- *****************************************************************************/
+ ******************************************************************************/
 
 /**
  *	Status codes used as return values as as_error.code values.
  */
 typedef enum as_status_e {
 
+	/***************************************************************************
+	 *	SUCCESS (all < 100)
+	 **************************************************************************/
+
 	/**
-	 *	Success
+	 *	Generic success.
 	 */
 	AEROSPIKE_OK							= 0,
 
+	/***************************************************************************
+	 *	ERRORS (all >= 100)
+	 **************************************************************************/
+
 	/**
-	 *	Generic Error
+	 *	Generic error.
 	 */
 	AEROSPIKE_ERR							= 100,
 
+	/***************************************************************************
+	 *	CLIENT API USAGE
+	 **************************************************************************/
+
 	/**
-	 *	Client usage error.
-	 *	This includes errors in configuring or using 
-	 *	the client.
+	 *	Generic client API usage error.
 	 */
 	AEROSPIKE_ERR_CLIENT					= 200,
 
 	/**
-	 *	Cluster state or connection error.
+	 *	Invalid client API parameter.
+	 */
+	AEROSPIKE_ERR_PARAM						= 201,
+
+	/***************************************************************************
+	 *	CLUSTER DISCOVERY & CONNECTION
+	 **************************************************************************/
+
+	/**
+	 *	Generic cluster discovery & connection error.
 	 */
 	AEROSPIKE_ERR_CLUSTER					= 300,
 
-	/**
-	 *	Write operation error.
-	 */
-	AEROSPIKE_ERR_WRITE						= 400,
-		AEROSPIKE_ERR_WRITE_GENERATION		= 401,
+	/***************************************************************************
+	 *	INCOMPLETE REQUESTS (i.e. NOT from server-returned error codes)
+	 **************************************************************************/
 
 	/**
-	 *	Read operation error.
+	 *	Request timed out.
 	 */
-	AEROSPIKE_ERR_READ						= 500,
+	AEROSPIKE_ERR_TIMEOUT					= 400,
 
 	/**
-	 *	Scan operation error.
+	 *	Request randomly dropped by client for throttling.
+	 *	@warning	Not yet supported.
 	 */
-	AEROSPIKE_ERR_SCAN						= 600,
+	AEROSPIKE_ERR_THROTTLED					= 401,
+
+	/***************************************************************************
+	 *	COMPLETED REQUESTS (all >= 500, from server-returned error codes)
+	 **************************************************************************/
 
 	/**
-	 *	Query operation error.
+	 *	Generic error returned by server.
 	 */
-	AEROSPIKE_ERR_QUERY						= 700,
-
-		/**
-		 *	The query was aborted.
-		 */
-		AEROSPIKE_ERR_QUERY_ABORTED 		= 701,
-
-		/**
-		 *	The result queue is full.
-		 */
-		AEROSPIKE_ERR_QUERY_QUEUEFULL 		= 702,
+	AEROSPIKE_ERR_SERVER					= 500,
 
 	/**
-	 *	Index operation error.
+	 *	Request protocol invalid, or invalid protocol field.
 	 */
-	AEROSPIKE_ERR_INDEX 					= 800,
-		AEROSPIKE_ERR_INDEX_KEY_NOTFOUND 	= 801,
-		AEROSPIKE_ERR_INDEX_TYPE_MISMATCH 	= 803,
-		AEROSPIKE_ERR_INDEX_NOTFOUND 		= 804,
-		AEROSPIKE_ERR_INDEX_OOM 			= 805,
-		AEROSPIKE_ERR_INDEX_GENERIC 		= 806,
-		AEROSPIKE_ERR_INDEX_EXISTS 			= 807,
-		AEROSPIKE_ERR_INDEX_SINGLEBIN_NS 	= 808,
-		AEROSPIKE_ERR_INDEX_UNKNOWN_TYPE 	= 809,
-		AEROSPIKE_ERR_INDEX_FOUND 			= 810,
-		AEROSPIKE_ERR_INDEX_NOTREADABLE 	= 811,
+	AEROSPIKE_ERR_REQUEST_INVALID			= 501,
 
 	/**
-	 *	UDF operation error.
+	 *	Namespace in request not found on server.
+	 *	@warning	Not yet supported, shows as AEROSPIKE_ERR_REQUEST_INVALID.
 	 */
-	AEROSPIKE_ERR_UDF						= 900,
+	AEROSPIKE_ERR_NAMESPACE_NOT_FOUND		= 502,
+
+	/**
+	 *	The server node is running out of memory and/or storage device space
+	 *	reserved for the specified namespace.
+	 */
+	AEROSPIKE_ERR_SERVER_FULL				= 503,
+
+	/**
+	 *	A cluster state change occurred during the request. This may also be
+	 *	returned by scan operations with the fail_on_cluster_change flag set.
+	 */
+	AEROSPIKE_ERR_CLUSTER_CHANGE			= 504,
+
+	/***************************************************************************
+	 *	RECORD-SPECIFIC
+	 **************************************************************************/
+
+	/**
+	 *	Generic record error.
+	 */
+	AEROSPIKE_ERR_RECORD					= 600,
+
+	/**
+	 *	Too may concurrent requests for one record - a "hot-key" situation.
+	 */
+	AEROSPIKE_ERR_RECORD_BUSY				= 601,
+
+	/**
+	 *	Record does not exist in database. May be returned by read, or write
+	 *	with policy AS_POLICY_EXISTS_UPDATE.
+	 *	@warning	AS_POLICY_EXISTS_UPDATE not yet supported.
+	 */
+	AEROSPIKE_ERR_RECORD_NOT_FOUND			= 602,
+
+	/**
+	 *	Record already exists. May be returned by write with policy
+	 *	AS_POLICY_EXISTS_CREATE.
+	 */
+	AEROSPIKE_ERR_RECORD_EXISTS				= 603,
+
+	/**
+	 *	Generation of record in database does not satisfy write policy.
+	 */
+	AEROSPIKE_ERR_RECORD_GENERATION			= 604,
+
+	/**
+	 *	Record being (re-)written can't fit in a storage write block.
+	 */
+	AEROSPIKE_ERR_RECORD_TOO_BIG			= 605,
+
+	/**
+	 *	Bin modification operation can't be done on an existing bin due to its
+	 *	value type.
+	 */
+	AEROSPIKE_ERR_BIN_INCOMPATIBLE_TYPE		= 606,
+
+	/***************************************************************************
+	 * XDR-SPECIFIC
+	 **************************************************************************/
+
+	/**
+	 * @TODO
+	 */
+	AEROSPIKE_ERR_NO_XDR					= 900,
+
+	/***************************************************************************
+	 *	SCAN OPERATIONS
+	 **************************************************************************/
+
+	/**
+	 *	Generic scan error.
+	 */
+	AEROSPIKE_ERR_SCAN						= 1000,
+
+	/**
+	 *	Scan aborted by user.
+	 */
+	AEROSPIKE_ERR_SCAN_ABORTED				= 1001,
+
+	/***************************************************************************
+	 *	QUERY OPERATIONS
+	 **************************************************************************/
+
+	/**
+	 *	Generic query error.
+	 */
+	AEROSPIKE_ERR_QUERY						= 1100,
+
+	/**
+	 * @TODO
+	 */
+	AEROSPIKE_ERR_QUERY_ABORTED 			= 1101,
+
+	/**
+	 * @TODO
+	 */
+	AEROSPIKE_ERR_QUERY_QUEUE_FULL 			= 1102,
+
+	/***************************************************************************
+	 *	SECONDARY INDEX OPERATIONS
+	 **************************************************************************/
+
+	/**
+	 *	Generic secondary index error.
+	 */
+	AEROSPIKE_ERR_INDEX 					= 1200,
+
+	// @TODO - finalize the order of errors in the rest of this section.
+
+	/**
+	 * @TODO
+	 */
+	AEROSPIKE_ERR_INDEX_KEY_NOT_FOUND,
+
+	/**
+	 * @TODO
+	 */
+	AEROSPIKE_ERR_INDEX_TYPE_MISMATCH,
+
+	/**
+	 * @TODO
+	 */
+	AEROSPIKE_ERR_INDEX_NOT_FOUND,
+
+	/**
+	 * @TODO
+	 */
+	AEROSPIKE_ERR_INDEX_OOM,
+
+	/**
+	 * @TODO
+	 */
+	AEROSPIKE_ERR_INDEX_EXISTS,
+
+	/**
+	 * @TODO
+	 */
+	AEROSPIKE_ERR_INDEX_SINGLE_BIN_NS,
+
+	/**
+	 * @TODO
+	 */
+	AEROSPIKE_ERR_INDEX_UNKNOWN_TYPE,
+
+	/**
+	 * @TODO
+	 */
+	AEROSPIKE_ERR_INDEX_FOUND,
+
+	/**
+	 * @TODO
+	 */
+	AEROSPIKE_ERR_INDEX_NOT_READABLE,
+
+	/***************************************************************************
+	 *	UDF OPERATIONS
+	 **************************************************************************/
+
+	/**
+	 *	Generic UDF error.
+	 */
+	AEROSPIKE_ERR_UDF						= 1300,
+
+	/**
+	 * @TODO
+	 */
+	AEROSPIKE_ERR_UDF_NOT_FOUND				= 1301
 
 } as_status;
-
-

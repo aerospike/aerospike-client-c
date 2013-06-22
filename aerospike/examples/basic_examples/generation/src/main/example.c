ae0b256ab1031d5c0dc434d095103e6247cb6c6a
Andrew Gooding <andy@aerospike.com>
2013-06-21 17:49:25 -0700

    Removed deprecated C client examples, added some new  basic examples.

---
+++ aerospike/examples/basic_examples/generation/src/main/example.c
@@ -0,0 +1,223 @@
+/*******************************************************************************
+ * Copyright 2008-2013 by Aerospike.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ ******************************************************************************/
+
+
+//==========================================================
+// Includes
+//
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include <aerospike/aerospike.h>
+#include <aerospike/aerospike_key.h>
+#include <aerospike/as_error.h>
+#include <aerospike/as_record.h>
+#include <aerospike/as_status.h>
+
+#include "example_utils.h"
+
+
+//==========================================================
+// Constants
+//
+
+const char TEST_BIN[] = "test-bin";
+
+
+//==========================================================
+// Forward Declarations
+//
+
+bool read_generation(aerospike* p_as, uint16_t* p_gen);
+
+
+//==========================================================
+// GENERATION Example
+//
+
+int
+main(int argc, char* argv[])
+{
+	// Parse command line arguments.
+	if (! example_get_opts(argc, argv, EXAMPLE_ALL_OPTS)) {
+		exit(-1);
+	}
+
+	// Connect to the aerospike database cluster.
+	aerospike as;
+	example_connect_to_aerospike(&as);
+
+	// Start clean.
+	example_remove_test_record(&as);
+
+	as_error err;
+
+	// Create a record object with one (integer value) bin.
+	as_record rec;
+	as_record_init(&rec, 1);
+	as_record_set_int64(&rec, TEST_BIN, 1001);
+
+	// Log its contents.
+	LOG("as_record object to write to database:");
+	example_dump_record(&rec);
+
+	// Write the record to the database. If the record isn't already in the
+	// database, it will be created with generation = 1.
+	if (aerospike_key_put(&as, &err, NULL, &g_key, &rec) != AEROSPIKE_OK) {
+		LOG("aerospike_key_put() returned %d - %s", err.code, err.message);
+		example_cleanup(&as, &rec);
+		exit(-1);
+	}
+
+	LOG("write succeeded");
+
+	uint16_t gen;
+
+	// Read the record back, and get its generation.
+	if (! read_generation(&as, &gen)) {
+		example_cleanup(&as, &rec);
+		exit(-1);
+	}
+
+	// Update the record object with a different bin value.
+	as_record_set_int64(&rec, TEST_BIN, 1002);
+
+	// Set its generation equal to that of the record in the database.
+	rec.gen = gen;
+
+	// Require that the next write will only succeed if generations match.
+	as_policy_write wpol;
+	as_policy_write_init(&wpol);
+	wpol.gen = AS_POLICY_GEN_EQ;
+
+	// Log its contents.
+	LOG("as_record object to write to database:");
+	example_dump_record(&rec);
+
+	// Re-write the record in the database. The write should succeed, and
+	// increment the generation.
+	if (aerospike_key_put(&as, &err, &wpol, &g_key, &rec) != AEROSPIKE_OK) {
+		LOG("aerospike_key_put() returned %d - %s", err.code, err.message);
+		example_cleanup(&as, &rec);
+		exit(-1);
+	}
+
+	LOG("re-write requiring generation = %u succeeded", rec.gen);
+
+	// Read the record back, and get its generation.
+	if (! read_generation(&as, &gen)) {
+		example_cleanup(&as, &rec);
+		exit(-1);
+	}
+
+	// Update the record object with a different bin value.
+	as_record_set_int64(&rec, TEST_BIN, 1003);
+
+	// Set its generation way past that of the record in the database.
+	rec.gen = gen + 10;
+
+	// Log its contents.
+	LOG("as_record object to write to database:");
+	example_dump_record(&rec);
+
+	// Try to re-write the record in the database. Use the same write policy,
+	// requiring generations to match. This write should fail.
+	if (aerospike_key_put(&as, &err, &wpol, &g_key, &rec) !=
+			AEROSPIKE_ERR_RECORD_GENERATION) {
+		LOG("aerospike_key_put() returned %d - %s, expected "
+				"AEROSPIKE_ERR_RECORD_GENERATION", err.code, err.message);
+		example_cleanup(&as, &rec);
+		exit(-1);
+	}
+
+	LOG("re-write requiring generation = %u failed as expected", rec.gen);
+
+	// Now require that the next write will only succeed if the specified
+	// generation is greater than that of the record in the database.
+	wpol.gen = AS_POLICY_GEN_GT;
+
+	// Log its contents.
+	LOG("as_record object to write to database:");
+	example_dump_record(&rec);
+
+	// Try again. This write should succeed, and increment the generation. (Note
+	// that it does not write the record with the local generation!)
+	if (aerospike_key_put(&as, &err, &wpol, &g_key, &rec) != AEROSPIKE_OK) {
+		LOG("aerospike_key_put() returned %d - %s", err.code, err.message);
+		example_cleanup(&as, &rec);
+		exit(-1);
+	}
+
+	LOG("re-write requiring generation < %u succeeded", rec.gen);
+
+	// Read the record back, and get its generation.
+	if (! read_generation(&as, &gen)) {
+		example_cleanup(&as, &rec);
+		exit(-1);
+	}
+
+	// Cleanup and disconnect from the database cluster.
+	example_cleanup(&as, &rec);
+
+	LOG("generation example successfully completed");
+
+	return 0;
+}
+
+
+//==========================================================
+// Helpers
+//
+
+bool
+read_generation(aerospike* p_as, uint16_t* p_gen)
+{
+	as_error err;
+	as_record* p_rec = NULL;
+
+	// Read the test record from the database.
+	if (aerospike_key_get(p_as, &err, NULL, &g_key, &p_rec) != AEROSPIKE_OK) {
+		LOG("aerospike_key_get() returned %d - %s", err.code, err.message);
+		return false;
+	}
+
+	// If we didn't get an as_record object back, something's wrong.
+	if (! p_rec) {
+		LOG("aerospike_key_get() retrieved null as_record object");
+		return false;
+	}
+
+	// Log the result.
+	LOG("record was successfully read from database:");
+	example_dump_record(p_rec);
+
+	// Return the generation.
+	*p_gen = p_rec->gen;
+
+	// Destroy the as_record object.
+	as_record_destroy(p_rec);
+
+	return true;
+}
---
+++ aerospike/examples/basic_examples/get/src/main/example.c
@@ -0,0 +1,177 @@
+/*******************************************************************************
+ * Copyright 2008-2013 by Aerospike.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ ******************************************************************************/
+
+
+//==========================================================
+// Includes
+//
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include <aerospike/aerospike.h>
+#include <aerospike/aerospike_key.h>
+#include <aerospike/as_error.h>
+#include <aerospike/as_record.h>
+#include <aerospike/as_status.h>
+
+#include "example_utils.h"
+
+
+//==========================================================
+// Forward Declarations
+//
+
+bool write_record(aerospike* p_as);
+
+
+//==========================================================
+// GET Example
+//
+
+int
+main(int argc, char* argv[])
+{
+	// Parse command line arguments.
+	if (! example_get_opts(argc, argv, EXAMPLE_ALL_OPTS)) {
+		exit(-1);
+	}
+
+	// Connect to the aerospike database cluster.
+	aerospike as;
+	example_connect_to_aerospike(&as);
+
+	// Start clean.
+	example_remove_test_record(&as);
+
+	as_error err;
+	as_record* p_rec = NULL;
+
+	// Try to read the test record from the database. This should fail since the
+	// record is not there.
+	if (aerospike_key_get(&as, &err, NULL, &g_key, &p_rec) !=
+			AEROSPIKE_ERR_RECORD_NOT_FOUND) {
+		LOG("aerospike_key_get() returned %d - %s, expected "
+				"AEROSPIKE_ERR_RECORD_NOT_FOUND", err.code, err.message);
+		example_cleanup(&as, p_rec);
+		exit(-1);
+	}
+
+	LOG("get (non-existent record) failed as expected");
+
+	// Write a record to the database so we can demonstrate read success.
+	if (! write_record(&as)) {
+		example_cleanup(&as, NULL);
+		exit(-1);
+	}
+
+	// Read the (whole) test record from the database.
+	if (aerospike_key_get(&as, &err, NULL, &g_key, &p_rec) != AEROSPIKE_OK) {
+		LOG("aerospike_key_get() returned %d - %s", err.code, err.message);
+		example_cleanup(&as, NULL);
+		exit(-1);
+	}
+
+	// Log the result and recycle the as_record object.
+	LOG("record was successfully read from database:");
+	example_dump_record(p_rec);
+	as_record_destroy(p_rec);
+	p_rec = NULL;
+
+	// Select bins 1 and 3 to read.
+	static const char* bins_1_3[] = { "test-bin-1", "test-bin-3", NULL };
+
+	// Read only these two bins of the test record from the database.
+	if (aerospike_key_select(&as, &err, NULL, &g_key, bins_1_3, &p_rec) !=
+			AEROSPIKE_OK) {
+		LOG("aerospike_key_select() returned %d - %s", err.code, err.message);
+		example_cleanup(&as, NULL);
+		exit(-1);
+	}
+
+	// Log the result and recycle the as_record object.
+	LOG("bins 1 and 3 were read from database:");
+	example_dump_record(p_rec);
+	as_record_destroy(p_rec);
+	p_rec = NULL;
+
+	// Select non-existent bin 4 to read.
+	static const char* bins_4[] = { "test-bin-4", NULL };
+
+	// Read only this bin from the database. This call should return an
+	// as_record object with one bin, with null as_bin_value.
+	if (aerospike_key_select(&as, &err, NULL, &g_key, bins_4, &p_rec) !=
+			AEROSPIKE_OK) {
+		LOG("aerospike_key_select() returned %d - %s", err.code, err.message);
+		example_cleanup(&as, NULL);
+		exit(-1);
+	}
+
+	// Log the result.
+	LOG("non-existent bin 4 was read from database:");
+	example_dump_record(p_rec);
+
+	// Cleanup and disconnect from the database cluster.
+	example_cleanup(&as, p_rec);
+
+	LOG("get example successfully completed");
+
+	return 0;
+}
+
+
+//==========================================================
+// Helpers
+//
+
+bool
+write_record(aerospike* p_as)
+{
+	as_error err;
+	as_record rec;
+
+	// Create an as_record object with three bins with different value types.
+	as_record_init(&rec, 3);
+	as_record_set_int64(&rec, "test-bin-1", 1111);
+	as_record_set_int64(&rec, "test-bin-2", 2222);
+	as_record_set_str(&rec, "test-bin-3", "test-bin-3-data");
+
+	// Log its contents.
+	LOG("as_record object to write to database:");
+	example_dump_record(&rec);
+
+	// Write the record to the database.
+	if (aerospike_key_put(p_as, &err, NULL, &g_key, &rec) != AEROSPIKE_OK) {
+		LOG("aerospike_key_put() returned %d - %s", err.code, err.message);
+		as_record_destroy(&rec);
+		return false;
+	}
+
+	LOG("write succeeded");
+
+	// Destroy the as_record object.
+	as_record_destroy(&rec);
+
+	return true;
+}

a8e4ca99e4614c192aba88b426d7ca6e23086c92
Andrew Gooding <andy@aerospike.com>
2013-07-08 11:15:15 -0700

    Adding query examples.

---
+++ aerospike/examples/query_examples/simple/src/main/example.c
@@ -0,0 +1,260 @@
+/*******************************************************************************
+ * Copyright 2008-2013 by Aerospike.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ ******************************************************************************/
+
+
+//==========================================================
+// Includes
+//
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include <aerospike/aerospike.h>
+#include <aerospike/aerospike_index.h>
+#include <aerospike/aerospike_key.h>
+#include <aerospike/aerospike_query.h>
+#include <aerospike/as_error.h>
+#include <aerospike/as_integer.h>
+#include <aerospike/as_query.h>
+#include <aerospike/as_record.h>
+#include <aerospike/as_status.h>
+#include <aerospike/as_val.h>
+
+#include "example_utils.h"
+
+
+//==========================================================
+// Constants
+//
+
+const char TEST_INDEX_NAME[] = "test-bin-index";
+
+
+//==========================================================
+// Forward Declarations
+//
+
+bool query_cb(const as_val* p_val, void* udata);
+void cleanup(aerospike* p_as);
+bool insert_records(aerospike* p_as);
+bool read_records(aerospike* p_as);
+void remove_records(aerospike* p_as);
+void remove_test_index(aerospike* p_as);
+
+
+//==========================================================
+// SIMPLE QUERY Example
+//
+
+int
+main(int argc, char* argv[])
+{
+	// Parse command line arguments.
+	if (! example_get_opts(argc, argv, EXAMPLE_MULTI_KEY_OPTS)) {
+		exit(-1);
+	}
+
+	// Connect to the aerospike database cluster.
+	aerospike as;
+	example_connect_to_aerospike(&as);
+
+	// Start clean.
+	remove_records(&as);
+	remove_test_index(&as);
+
+	as_error err;
+
+	// Create a numeric secondary index on test-bin.
+	if (aerospike_index_integer_create(&as, &err, NULL, g_namespace, g_set,
+			"test-bin", TEST_INDEX_NAME) != AEROSPIKE_OK) {
+		LOG("aerospike_index_integer_create() returned %d - %s", err.code,
+				err.message);
+		cleanup(&as);
+		exit(-1);
+	}
+
+	if (! insert_records(&as)) {
+		cleanup(&as);
+		exit(-1);
+	}
+
+	if (! read_records(&as)) {
+		cleanup(&as);
+		exit(-1);
+	}
+
+	// Create an as_query object.
+	as_query query;
+	as_query_init(&query, g_namespace, g_set);
+
+	// Generate an as_query.where condition.
+	as_query_where_inita(&query, 1);
+	as_query_where(&query, "test-bin", integer_equals(7));
+
+	LOG("executing query: where test-bin = 7");
+
+	// Execute the query. This call blocks - callbacks are made in the scope of
+	// this call.
+	if (aerospike_query_foreach(&as, &err, NULL, &query, query_cb, NULL) !=
+			AEROSPIKE_OK) {
+		LOG("aerospike_query_foreach() returned %d - %s", err.code,
+				err.message);
+		as_query_destroy(&query);
+		cleanup(&as);
+	}
+
+	LOG("query executed");
+
+	as_query_destroy(&query);
+
+	// Cleanup and disconnect from the database cluster.
+	cleanup(&as);
+
+	LOG("simple query example successfully completed");
+
+	return 0;
+}
+
+
+//==========================================================
+// Query Callback
+//
+
+bool
+query_cb(const as_val* p_val, void* udata)
+{
+	if (! p_val) {
+		LOG("query callback returned null - query is complete");
+		return true;
+	}
+
+	// The query didn't use a UDF, so the as_val object should be an as_record.
+	LOG("query callback returned record:");
+	example_dump_record(as_record_fromval(p_val));
+
+	// Caller's responsibility to destroy as_val returned.
+	as_val_destroy(p_val);
+
+	return true;
+}
+
+
+//==========================================================
+// Helpers
+//
+
+void
+cleanup(aerospike* p_as)
+{
+	remove_records(p_as);
+	remove_test_index(p_as);
+	example_cleanup(p_as);
+}
+
+bool
+insert_records(aerospike* p_as)
+{
+	as_key key;
+	as_record rec;
+
+	// Create an as_record object with one (integer value) bin.
+	as_record_inita(&rec, 1);
+
+	// Re-using key and rec, write records into the database such that each
+	// record's key and (test-bin) value is based on the loop index.
+	for (uint32_t i = 0; i < g_n_keys; i++) {
+		as_key_init_int64(&key, g_namespace, g_set, (int64_t)i);
+		as_record_set_int64(&rec, "test-bin", (int64_t)i);
+
+		as_error err;
+
+		// Write a record to the database.
+		if (aerospike_key_put(p_as, &err, NULL, &key, &rec) != AEROSPIKE_OK) {
+			LOG("aerospike_key_put() returned %d - %s", err.code, err.message);
+			return false;
+		}
+	}
+
+	LOG("insert succeeded");
+
+	return true;
+}
+
+bool
+read_records(aerospike* p_as)
+{
+	as_key key;
+
+	for (uint32_t i = 0; i < g_n_keys; i++) {
+		as_key_init_int64(&key, g_namespace, g_set, (int64_t)i);
+
+		as_error err;
+		as_record* p_rec = NULL;
+
+		// Read a test record from the database.
+		if (aerospike_key_get(p_as, &err, NULL, &key, &p_rec) != AEROSPIKE_OK) {
+			LOG("aerospike_key_get() returned %d - %s", err.code, err.message);
+			return false;
+		}
+
+		// If we didn't get an as_record object back, something's wrong.
+		if (! p_rec) {
+			LOG("aerospike_key_get() retrieved null as_record object");
+			return false;
+		}
+
+		// Log the result.
+		LOG("read key %u from database:", i);
+		example_dump_record(p_rec);
+
+		// Destroy the as_record object.
+		as_record_destroy(p_rec);
+	}
+
+	return true;
+}
+
+void
+remove_records(aerospike* p_as)
+{
+	as_key key;
+
+	for (uint32_t i = 0; i < g_n_keys; i++) {
+		as_key_init_int64(&key, g_namespace, g_set, (int64_t)i);
+
+		as_error err;
+
+		// Ignore errors - just trying to leave the database as we found it.
+		aerospike_key_remove(p_as, &err, NULL, &key);
+	}
+}
+
+void
+remove_test_index(aerospike* p_as)
+{
+	as_error err;
+
+	// Ignore errors - just trying to leave the database as we found it.
+	aerospike_index_remove(p_as, &err, NULL, g_namespace, TEST_INDEX_NAME);
+}

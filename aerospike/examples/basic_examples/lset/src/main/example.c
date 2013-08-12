384191cca2a96299f03a19642444cd157cc38014
Wei-Ling Chu <weiling@aerospike.com>
2013-08-12 16:40:26 -0700

    beautify examples to andy's standard. Add ldt example blurb into README

--- aerospike/examples/basic_examples/lset/src/main/example.c
+++ aerospike/examples/basic_examples/lset/src/main/example.c
@@ -33,24 +33,15 @@
 #include <aerospike/aerospike.h>
 #include <aerospike/aerospike_key.h>
 #include <aerospike/aerospike_lset.h>
+#include <aerospike/as_arraylist.h>
+#include <aerospike/as_arraylist_iterator.h>
 #include <aerospike/as_error.h>
+#include <aerospike/as_ldt.h>
+#include <aerospike/as_list.h>
 #include <aerospike/as_record.h>
 #include <aerospike/as_status.h>
-#include <aerospike/as_ldt.h>
-#include <aerospike/as_arraylist_iterator.h>
 
 #include "example_utils.h"
-
-
-//==========================================================
-// Constants
-//
-
-
-//==========================================================
-// Forward Declarations
-//
-
 
 
 //==========================================================
@@ -67,179 +58,181 @@ main(int argc, char* argv[])
 
 	// Connect to the aerospike database cluster.
 	aerospike as;
-
 	example_connect_to_aerospike(&as);
 
 	// Start clean.
 	example_remove_test_record(&as);
 
-	// Create a lset bin to use.
 	as_ldt lset;
+
+	// Create a lset bin to use. No need to destroy as_ldt if using
+	// as_ldt_init() on stack object.
 	if (! as_ldt_init(&lset, "mylset", AS_LDT_LSET, NULL)) {
 		LOG("unable to initialize ldt");
 		exit(-1);
 	}
 
-	// Push a few values into the set.
 	as_error err;
+
+	// No need to destroy as_integer if using as_integer_init() on stack object.
 	as_integer ival;
 	as_integer_init(&ival, 12345);
 
-	if ( aerospike_lset_add(&as, &err, NULL, &g_key, &lset, (as_val *) &ival) != AEROSPIKE_OK ) {
-		LOG("first aerospike_set_add() returned %d - %s", err.code, err.message);
+	// Add an integer value to the set.
+	if (aerospike_lset_add(&as, &err, NULL, &g_key, &lset,
+			(const as_val*)&ival) != AEROSPIKE_OK) {
+		LOG("first aerospike_set_add() returned %d - %s", err.code,
+				err.message);
 		exit(-1);
 	}
 
+	// No need to destroy as_string if using as_string_init() on stack object.
 	as_string sval;
 	as_string_init(&sval, "lset value", false);
-	if ( aerospike_lset_add(&as, &err, NULL, &g_key, &lset, (as_val *) &sval) != AEROSPIKE_OK ) {
-		LOG("second aerospike_set_add() returned %d - %s", err.code, err.message);
+
+	// Add a string value to the set.
+	if (aerospike_lset_add(&as, &err, NULL, &g_key, &lset,
+			(const as_val*)&sval) != AEROSPIKE_OK) {
+		LOG("second aerospike_set_add() returned %d - %s", err.code,
+				err.message);
 		exit(-1);
 	}
+
 	LOG("2 values added to set");
 
-	// Look at the lset size right now
-	uint32_t n = 0;
-	if ( aerospike_lset_size(&as, &err, NULL, &g_key, &lset, &n) != AEROSPIKE_OK ) {
+	uint32_t n_elements = 0;
+
+	// See how many elements we have in the set now.
+	if (aerospike_lset_size(&as, &err, NULL, &g_key, &lset, &n_elements)
+			!= AEROSPIKE_OK) {
 		LOG("aerospike_lset_size() returned %d - %s", err.code, err.message);
 		exit(-1);
 	}
-	if (n != 2) {
-		LOG("lset size not as expected %d not 2",n);
+
+	if (n_elements != 2) {
+		LOG("unexpected lset size %u", n_elements);
 		exit(-1);
 	}
-	LOG("lset size confirmed to be %d",n);
 
-	// Get all the values back.
+	LOG("lset size confirmed to be %u", n_elements);
+
 	as_ldt lset2;
 	as_ldt_init(&lset2, "mylset", AS_LDT_LSET, NULL);
 
-	as_list *list = NULL;
+	as_list* p_list = NULL;
 
-	if ( aerospike_lset_filter(&as, &err, NULL, &g_key, &lset, NULL, NULL, &list) != AEROSPIKE_OK ) {
- 		LOG("error(%d) %s at [%s:%d]", err.code, err.message, err.file, err.line);
-		as_list_destroy(list);
+	// Get all the values back.
+	if (aerospike_lset_filter(&as, &err, NULL, &g_key, &lset, NULL, NULL,
+			&p_list) != AEROSPIKE_OK) {
+		LOG("aerospike_lset_filter() returned %d - %s", err.code, err.message);
+		as_list_destroy(p_list);
 		exit(-1);
- 	}
-
-	// See if the elements match what we expect
-	as_arraylist_iterator it;
-	as_arraylist_iterator_init(&it, (as_arraylist *)list);
-	while ( as_arraylist_iterator_has_next(&it) ) {
-		const as_val * val = as_arraylist_iterator_next(&it);
-		LOG(" Peek - type = %d value = %s ", as_val_type(val), as_val_tostring(val));
 	}
-	as_list_destroy(list);
-	list = NULL;
 
-	// Add 3 more items into the set
+	// See if the elements match what we expect.
+	as_arraylist_iterator it;
+	as_arraylist_iterator_init(&it, (const as_arraylist*)p_list);
+
+	while (as_arraylist_iterator_has_next(&it)) {
+		const as_val* p_val = as_arraylist_iterator_next(&it);
+		LOG("   element - type = %d, value = %s ", as_val_type(p_val),
+				as_val_tostring(p_val));
+	}
+
+	as_list_destroy(p_list);
+	p_list = NULL;
+
+	// Add 3 more items into the set. By using as_arraylist_inita(), we won't
+	// need to destroy the as_arraylist if we only use
+	// as_arraylist_append_int64().
 	as_arraylist vals;
 	as_arraylist_inita(&vals, 3);
 	as_arraylist_append_int64(&vals, 1001);
 	as_arraylist_append_int64(&vals, 2002);
 	as_arraylist_append_int64(&vals, 3003);
 
-	if ( aerospike_lset_addall(&as, &err, NULL, &g_key, &lset, (as_list *) &vals) != AEROSPIKE_OK ) {
+	if (aerospike_lset_addall(&as, &err, NULL, &g_key, &lset,
+			(const as_list*)&vals) != AEROSPIKE_OK) {
 		LOG("aerospike_lset_addall() returned %d - %s", err.code, err.message);
 		exit(-1);
 	}
-	LOG("3 more values added");
 
+	LOG("3 more values added");
 
 	// Get and print all the values back again.
-	if ( aerospike_lset_filter(&as, &err, NULL, &g_key, &lset, NULL, NULL, &list) != AEROSPIKE_OK ) {
- 		LOG("error(%d) %s at [%s:%d]", err.code, err.message, err.file, err.line);
-		as_list_destroy(list);
+	if (aerospike_lset_filter(&as, &err, NULL, &g_key, &lset, NULL, NULL,
+			&p_list) != AEROSPIKE_OK) {
+		LOG("second aerospike_lset_filter() returned %d - %s", err.code,
+				err.message);
+		as_list_destroy(p_list);
 		exit(-1);
- 	}
-	as_arraylist_iterator_init(&it, (as_arraylist *)list);
-	while ( as_arraylist_iterator_has_next(&it) ) {
-		const as_val * val = as_arraylist_iterator_next(&it);
-		LOG(" Peek - type = %d value = %s ", as_val_type(val), as_val_tostring(val));
 	}
-	as_list_destroy(list);
-	list = NULL;
 
-	// Check if a specific value exists
+	as_arraylist_iterator_init(&it, (const as_arraylist*)p_list);
+
+	while (as_arraylist_iterator_has_next(&it)) {
+		const as_val* p_val = as_arraylist_iterator_next(&it);
+		LOG("   element - type = %d, value = %s ", as_val_type(p_val),
+				as_val_tostring(p_val));
+	}
+
+	as_list_destroy(p_list);
+	p_list = NULL;
+
+	// No need to destroy as_boolean if using as_boolean_init() on stack object.
 	as_boolean exists;
-	as_boolean_init(&exists,false);
-	if ( aerospike_lset_exists(&as, &err, NULL, &g_key, &lset2, (as_val *)&ival, &exists) != AEROSPIKE_OK ) {
- 		LOG("error(%d) %s at [%s:%d]", err.code, err.message, err.file, err.line);
- 		exit (-1);
-	}
-	if (as_boolean_get(&exists)) {
- 		LOG("not able to find a value which should be in the set");
- 		exit (-1);
+	as_boolean_init(&exists, false);
+
+	// Check if a specific value exists.
+	if (aerospike_lset_exists(&as, &err, NULL, &g_key, &lset2,
+			(const as_val*)&ival, &exists) != AEROSPIKE_OK) {
+		LOG("aerospike_lset_exists() returned %d - %s", err.code, err.message);
+		exit(-1);
 	}
 
-	// Check if a non-existent value, isn't
-	as_boolean_init(&exists,false);
+	if (as_boolean_get(&exists)) {
+		LOG("not able to find a value which should be in the set");
+		exit(-1);
+	}
+
+	as_boolean_init(&exists, false);
 	as_integer_init(&ival, 33333);
-	if ( aerospike_lset_exists(&as, &err, NULL, &g_key, &lset2, (as_val *)&ival, &exists) != AEROSPIKE_OK ) {
- 		LOG("error(%d) %s at [%s:%d]", err.code, err.message, err.file, err.line);
- 		exit (-1);
+
+	// Check that a value which should not be in the set, really isn't.
+	if (aerospike_lset_exists(&as, &err, NULL, &g_key, &lset2,
+			(const as_val*)&ival, &exists) != AEROSPIKE_OK) {
+		LOG("second aerospike_lset_exists() returned %d - %s", err.code,
+				err.message);
+		exit(-1);
 	}
+
 	if (as_boolean_get(&exists)) {
- 		LOG("not able to find a value which should be in the set");
- 		exit (-1);
-	}
-	LOG("Existence function checked");
-
-	/*
-	// Add 3 more items onto the list
-	as_arraylist vals;
-	as_arraylist_inita(&vals, 3);
-	as_arraylist_append_int64(&vals, 888);
-	as_arraylist_append_int64(&vals, 999);
-	as_arraylist_append_int64(&vals, 1000);
-
-	if ( aerospike_lset_addall(&as, &err, NULL, &g_key, &lset, (as_list *) &vals) != AEROSPIKE_OK ) {
-		LOG("second aerospike_lset_addall() returned %d - %s", err.code, err.message);
+		LOG("found a value which should not be in the set");
 		exit(-1);
 	}
-	LOG("3 more values added");
 
-	// Peek all the values back again
-	as_ldt_init(&lset2, "mylset", AS_LDT_LSET, NULL);
+	LOG("existence functionality checked");
 
-	if ( aerospike_lset_filter(&as, &err, NULL, &g_key, &lset, NULL, NULL, &list) != AEROSPIKE_OK ) {
- 		LOG("error(%d) %s at [%s:%d]", err.code, err.message, err.file, err.line);
-		as_list_destroy(list);
+	// Destroy the lset.
+	if (aerospike_lset_destroy(&as, &err, NULL, &g_key, &lset) !=
+			AEROSPIKE_OK) {
+		LOG("aerospike_lset_destroy() returned %d - %s", err.code, err.message);
 		exit(-1);
- 	}
-
-	// See if the elements match what we expect
-	as_arraylist_iterator_init(&it, (as_arraylist *)list);
-	while ( as_arraylist_iterator_has_next(&it) ) {
-		const as_val * val = as_arraylist_iterator_next(&it);
-		LOG(" Peek - type = %d value = %s ", as_val_type(val), as_val_tostring(val));
 	}
-	as_list_destroy(list);
-	*/
-	// Destroy the Lset
-	LOG("Deleting Lset");
-	if ( aerospike_lset_destroy(&as, &err, NULL, &g_key, &lset) != AEROSPIKE_OK ) {
- 		LOG("error(%d) %s", err.code, err.message);
-		exit(-1);
- 	}
 
-	// See if we can still do any lset operations
-	n = 0;
-	if ( aerospike_lset_size(&as, &err, NULL, &g_key, &lset, &n) == AEROSPIKE_OK ) {
+	n_elements = 0;
+
+	// See if we can still do any lset operations.
+	if (aerospike_lset_size(&as, &err, NULL, &g_key, &lset, &n_elements) ==
+			AEROSPIKE_OK) {
 		LOG("aerospike_lset_size() did not return error");
 		exit(-1);
 	}
 
 	// Cleanup and disconnect from the database cluster.
-	// Note: deletes example record used to keep database intact
 	example_cleanup(&as);
 
 	LOG("lset example successfully completed");
 
 	return 0;
 }
-
-
-//==========================================================
-// Helpers
-//

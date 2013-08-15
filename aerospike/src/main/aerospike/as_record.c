b930650479efeea4ff7d7ba007a376f6798680e9
Chris Stivers <chris@stivers.us>
2013-08-15 11:43:17 -0700

    Added functions to improve flexibility in values for keys and records.

--- aerospike/src/main/aerospike/as_record.c
+++ aerospike/src/main/aerospike/as_record.c
@@ -254,7 +254,23 @@ bool as_record_set_str(as_record * rec, const as_bin_name name, const char * val
 {
 	as_bin * bin = as_record_bin_forupdate(rec, name);
 	if ( !bin ) return false;
-	as_bin_init_str(bin, name, value);
+	as_bin_init_str(bin, name, value, false);
+	return true;
+}
+
+/**
+ *	Set specified bin's value to an NULL terminated string.
+ *	as_record_set_str(rec, "bin", "abc");
+ *	@param rec 	- the record containing the bin
+ *	@param name 	- the name of the bin
+ *	@param value - the value of the bin
+ *	@return true on success, false on failure.
+ */
+bool as_record_set_strp(as_record * rec, const as_bin_name name, const char * value, bool free) 
+{
+	as_bin * bin = as_record_bin_forupdate(rec, name);
+	if ( !bin ) return false;
+	as_bin_init_str(bin, name, value, free);
 	return true;
 }
 
@@ -272,7 +288,25 @@ bool as_record_set_raw(as_record * rec, const as_bin_name name, const uint8_t * 
 {
 	as_bin * bin = as_record_bin_forupdate(rec, name);
 	if ( !bin ) return false;
-	as_bin_init_raw(bin, name, value, size);
+	as_bin_init_raw(bin, name, value, size, false);
+	return true;
+}
+
+/**
+ *	Set specified bin's value to raw bytes of given length.
+ *	uint8_t bytes[3] = {1,2,3}
+ *	as_record_set_raw(rec, "bin", bytes, 3);
+ *	@param rec 	- the record containing the bin
+ *	@param name 	- the name of the bin
+ *	@param value - the value of the bin
+ *	@param size 	- the size of the value
+ *	@return true on success, false on failure.
+ */
+bool as_record_set_rawp(as_record * rec, const as_bin_name name, const uint8_t * value, uint32_t size, bool free) 
+{
+	as_bin * bin = as_record_bin_forupdate(rec, name);
+	if ( !bin ) return false;
+	as_bin_init_raw(bin, name, value, size, free);
 	return true;
 }
 

742517c09ed27bb01657a0d75a5b05c0994ee3a2
Wei-Ling Chu <weiling@aerospike.com>
2013-08-19 11:46:16 -0700

    basic C API for llist support

---
+++ aerospike/src/include/aerospike/aerospike_llist.h
@@ -0,0 +1,295 @@
+/******************************************************************************
+ *	Copyright 2008-2013 by Aerospike.
+ *
+ *	Permission is hereby granted, free of charge, to any person obtaining a copy 
+ *	of this software and associated documentation files (the "Software"), to 
+ *	deal in the Software without restriction, including without limitation the 
+ *	rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+ *	sell copies of the Software, and to permit persons to whom the Software is 
+ *	furnished to do so, subject to the following conditions:
+ *	
+ *	The above copyright notice and this permission notice shall be included in 
+ *	all copies or substantial portions of the Software.
+ *	
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ *	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ *	IN THE SOFTWARE.
+ *****************************************************************************/
+
+/**
+ * Functionality related to Large List Data Type
+ */
+
+#pragma once 
+
+#include <aerospike/aerospike.h>
+#include <aerospike/as_error.h>
+#include <aerospike/as_ldt.h>
+#include <aerospike/as_list.h>
+#include <aerospike/as_operations.h>
+#include <aerospike/as_policy.h>
+#include <aerospike/as_status.h>
+#include <aerospike/as_key.h>
+#include <aerospike/as_val.h>
+#include <aerospike/as_boolean.h>
+
+/******************************************************************************
+ *	FUNCTIONS
+ *****************************************************************************/
+
+/**
+ *	Add a value into the llist.
+ *
+ *	~~~~~~~~~~{.c}
+ *	as_key key;
+ *	as_key_init(&key, "myns", "myset", "mykey");
+ *
+ *	as_ldt llist;
+ *	as_ldt_init(&llist, "myllist", AS_LDT_LLIST, NULL);
+ *	
+ *	as_integer ival;
+ *	as_integer_init(&ival, 123);
+ *
+ *	if ( aerospike_llist_add(&as, &err, NULL, &key, &llist, (as_val *) &ival) != AEROSPIKE_OK ) {
+ *		fprintf(stderr, "error(%d) %s at [%s:%d]", err.code, err.message, err.file, err.line);
+ *	}
+ *	~~~~~~~~~~
+ *
+ *	@param as			The aerospike instance to use for this operation.
+ *	@param err			The as_error to be populated if an error occurs.
+ *	@param policy		The policy to use for this operation. If NULL, then the default policy will be used.
+ *	@param key			The key of the record.
+ *	@param ldt 			The ldt bin to insert values to.
+ *	@param val			The value to insert into the llist.
+ *
+ *	@return AEROSPIKE_OK if successful. Otherwise an error.
+ *
+ *	@ingroup ldt_operations
+ */
+as_status aerospike_llist_add(
+	aerospike * as, as_error * err, const as_policy_apply * policy,
+	const as_key * key, const as_ldt * ldt, const as_val * val);
+
+/**
+ *	Add a list of values into the llist.
+ *
+ *	~~~~~~~~~~{.c}
+ *	as_key key;
+ *	as_key_init(&key, "myns", "myset", "mykey");
+ *
+ *	as_ldt llist;
+ *	as_ldt_init(&llist, "myllist", AS_LDT_LLIST, NULL);
+ *
+ *
+ *	as_arraylist vals;
+ *	as_arraylist_inita(&vals, 2);
+ *	as_string s;
+ *	as_string_init(s,"a string",false);
+ *	as_arraylist_append_string(&vals, s);
+ *	as_arraylist_append_int64(&vals, 35);
+ *
+ *	if ( aerospike_llist_addall(&as, &err, NULL, &key, &llist, (as_list *)vals) != AEROSPIKE_OK ) {
+ *		fprintf(stderr, "error(%d) %s at [%s:%d]", err.code, err.message, err.file, err.line);
+ *	}
+ *
+ *	~~~~~~~~~~
+ *
+ *	@param as			The aerospike instance to use for this operation.
+ *	@param err			The as_error to be populated if an error occurs.
+ *	@param policy		The policy to use for this operation. If NULL, then the default policy will be used.
+ *	@param key			The key of the record.
+ *	@param ldt 			The ldt bin to insert values to.
+ *	@param vals			The list of values to insert into the llist.
+ *
+ *	@return AEROSPIKE_OK if successful. Otherwise an error.
+ *
+ *	@ingroup ldt_operations
+ */
+as_status aerospike_llist_addall(
+	aerospike * as, as_error * err, const as_policy_apply * policy,
+	const as_key * key, const as_ldt * ldt, const as_list * vals);
+
+/**
+ *	See if a value exists in an llist
+ *
+ *	~~~~~~~~~~{.c}
+ *	as_key key;
+ *	as_key_init(&key, "myns", "myset", "mykey");
+ *
+ *	as_ldt llist;
+ *	as_ldt_init(&llist, "myllist", AS_LDT_LLIST, NULL);
+ *
+ *	as_integer ival;
+ *	as_integer_init(&ival, 123);
+ *
+ *  boolean exists = false;
+ *	
+ *	if ( aerospike_llist_exists(&as, &err, NULL, &key, &llist, &ival, &exists) != AEROSPIKE_OK ) {
+ *		fprintf(stderr, "error(%d) %s at [%s:%d]", err.code, err.message, err.file, err.line);
+ *	}
+ *	else {
+ *		// do logic because element exists
+ *	}
+ *	~~~~~~~~~~
+ *
+ *	@param as			The aerospike instance to use for this operation.
+ *	@param err			The as_error to be populated if an error occurs.
+ *	@param policy		The policy to use for this operation. If NULL, then the default policy will be used.
+ *	@param key			The key of the record.
+ *	@param ldt 			The llist bin to lookup from. If not an llist bin, will return error.
+ *	@param exists 		Returned boolean value to indicate value exists.
+ *
+ *	@return AEROSPIKE_OK if successful. Otherwise an error.
+ *	
+ *	@ingroup ldt_operations
+ */
+
+as_status aerospike_llist_exists(
+	aerospike * as, as_error * err, const as_policy_apply * policy,
+	const as_key * key, const as_ldt * ldt, const as_val * val,
+	as_boolean *exists);
+
+/**
+ *	Given an llist bin, filter the set of objects using the given filter function.
+ *	If no filter function is specified, all values in the set will be returned.
+ *
+ *	~~~~~~~~~~{.c}
+ *	as_key key;
+ *	as_key_init(&key, "myns", "myset", "mykey");
+ *
+ *	as_ldt llist;
+ *	as_ldt_init(&llist, "myllist", AS_LDT_LLIST, NULL);
+ *
+ *	as_list *list = NULL;
+ *
+ *	if ( aerospike_llist_filter(&as, &err, NULL, &key, &llist,
+ *			"search_filter", NULL, (as_list *) &list) != AEROSPIKE_OK ) {
+ *		fprintf(stderr, "error(%d) %s at [%s:%d]", err.code, err.message, err.file, err.line);
+ *	}
+ *	else {
+ *		// process the returned elements
+ *		as_arraylist_destroy(list);
+ *	}
+ *	~~~~~~~~~~
+ *
+ *	@param as			The aerospike instance to use for this operation.
+ *	@param err			The as_error to be populated if an error occurs.
+ *	@param policy		The policy to use for this operation. If NULL, then the default policy will be used.
+ *	@param key			The key of the record.
+ *	@param ldt 			The llist bin to search from. If not an llist bin, will return error.
+ *	@param filter		The name of the User-Defined-Function to use as a search filter.
+ *	@param fargs		The list of parameters passed in to the User-Defined-Function filter.
+ *	@param list			The pointer to a list of elements returned from search function. Pointer should
+ *						be NULL passed in.
+ *
+ *	@return AEROSPIKE_OK if successful. Otherwise an error.
+ *
+ *	@ingroup ldt_operations
+ */
+as_status aerospike_llist_filter(
+	aerospike * as, as_error * err, const as_policy_apply * policy,
+	const as_key * key, const as_ldt * ldt,
+	const as_udf_function_name filter, const as_list *filter_args,
+	as_list ** elements );
+
+/**
+ *	Look up a llist and find how many elements it contains
+ *
+ *	~~~~~~~~~~{.c}
+ *	as_key key;
+ *	as_key_init(&key, "myns", "myset", "mykey");
+ *
+ *	as_ldt llist;
+ *	as_ldt_init(&llist, "myllist", AS_LDT_LLIST, NULL);
+ *	uint32_t llist_size = 0;
+ *
+ *	if ( aerospike_llist_size(&as, &err, NULL, &key, &llist, &llist_size) != AEROSPIKE_OK ) {
+ *		fprintf(stderr, "error(%d) %s at [%s:%d]", err.code, err.message, err.file, err.line);
+ *	}
+ *	~~~~~~~~~~
+ *
+ *	@param as			The aerospike instance to use for this operation.
+ *	@param err			The as_error to be populated if an error occurs.
+ *	@param policy		The policy to use for this operation. If NULL, then the default policy will be used.
+ *	@param key			The key of the record.
+ *	@param ldt 			The llist to operate on. If not an llist bin, will return error.
+ *	@param n			Return the number of elements in the llist.
+ *
+ *	@return AEROSPIKE_OK if successful. Otherwise an error.
+ *
+ *	@ingroup ldt_operations
+ */
+as_status aerospike_llist_size(
+	aerospike * as, as_error * err, const as_policy_apply * policy,
+	const as_key * key, const as_ldt * ldt,
+	uint32_t *n
+	);
+
+/**
+ *	Delete the given value from the llist
+ *
+ *	~~~~~~~~~~{.c}
+ *	as_key key;
+ *	as_key_init(&key, "myns", "myset", "mykey");
+ *
+ *	as_ldt llist;
+ *	as_ldt_init(&llist, "llist", AS_LDT_LLIST, NULL);
+ *
+ *	as_integer ival;
+ *	as_integer_init(&ival, 123);
+ *
+ *	if ( aerospike_llist_remove(&as, &err, NULL, &key, &llist, &ival) != AEROSPIKE_OK ) {
+ *		fprintf(stderr, "error(%d) %s at [%s:%d]", err.code, err.message, err.file, err.line);
+ *	}
+ *	~~~~~~~~~~
+ *
+ *	@param as			The aerospike instance to use for this operation.
+ *	@param err			The as_error to be populated if an error occurs.
+ *	@param policy		The policy to use for this operation. If NULL, then the default policy will be used.
+ *	@param key			The key of the record.
+ *	@param ldt 			The llist bin to delete from. If not an llist bin, will return error.
+ *	@param val			The value to delete from the set.
+ *
+ *	@return AEROSPIKE_OK if successful. Otherwise an error.
+ *
+ *	@ingroup ldt_operations
+ */
+as_status aerospike_llist_remove(
+	aerospike * as, as_error * err, const as_policy_apply * policy,
+	const as_key * key, const as_ldt * ldt, const as_val *element
+	);
+
+/**
+ *	Destroy the llist bin
+ *
+ *	~~~~~~~~~~{.c}
+ *	as_key key;
+ *	as_key_init(&key, "myns", "myset", "mykey");
+ *
+ *	as_ldt llist;
+ *	as_ldt_init(&llist, "myllist", AS_LDT_LLIST, NULL);
+ *
+ *	if ( aerospike_llist_destroy(&as, &err, NULL, &key, &llist) != AEROSPIKE_OK ) {
+ *		fprintf(stderr, "error(%d) %s at [%s:%d]", err.code, err.message, err.file, err.line);
+ *	}
+ *	~~~~~~~~~~
+ *
+ *	@param as			The aerospike instance to use for this operation.
+ *	@param err			The as_error to be populated if an error occurs.
+ *	@param policy		The policy to use for this operation. If NULL, then the default policy will be used.
+ *	@param key			The key of the record.
+ *	@param ldt 			The llist bin to destroy. If not an llist bin, will return error.
+ *
+ *	@return AEROSPIKE_OK if successful. Otherwise an error.
+ *
+ *	@ingroup ldt_operations
+ */
+as_status aerospike_llist_destroy(
+	aerospike * as, as_error * err, const as_policy_apply * policy,
+	const as_key * key, const as_ldt * ldt
+	);
+
---
+++ aerospike/src/main/aerospike/aerospike_llist.c
@@ -0,0 +1,368 @@
+/******************************************************************************
+ * Copyright 2008-2013 by Aerospike.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to 
+ * deal in the Software without restriction, including without limitation the 
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+ * sell copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *****************************************************************************/
+#include <aerospike/aerospike.h>
+#include <aerospike/aerospike_llist.h>
+#include <aerospike/aerospike_key.h>
+#include <aerospike/as_ldt.h>
+#include <aerospike/as_error.h>
+#include <aerospike/as_policy.h>
+#include <aerospike/as_status.h>
+#include <aerospike/as_types.h>
+
+#include "_log.h"
+#include "_ldt.h"
+
+// ++==============++
+// || Fixed Values ||
+// ++==============++
+// The names of the Lua Functions that implement Large Set Ops
+static char * LDT_LIST_OP_ADD				= "llist_insert";
+static char * LDT_LIST_OP_ADDALL			= "llist_insert_all";
+static char * LDT_LIST_OP_SIZE       		= "llist_size";
+static char * LDT_LIST_OP_EXISTS     		= "llist_exists";
+static char * LDT_LIST_OP_SCAN       		= "llist_scan";
+static char * LDT_LIST_OP_SCAN_WITH_FILTER	= "llist_scan_then_filter";
+static char * LDT_LIST_OP_REMOVE     		= "llist_delete";
+static char * LDT_LIST_OP_DESTROY  			= "llist_remove";
+
+
+static as_status aerospike_llist_add_internal(
+	aerospike * as, as_error * err, const as_policy_apply * policy,
+	const as_key * key, const as_ldt * ldt, const as_val *val,  const char *operation)
+{
+	if ( !err ) {
+		return AEROSPIKE_ERR_PARAM;
+	}
+	as_error_reset(err);
+
+	if (!as || !key || !ldt) {
+		return as_error_set(err, AEROSPIKE_ERR_PARAM, "invalid parameter. "
+				"as/key/ldt/n cannot be null");
+	}
+	if (ldt->type != AS_LDT_LLIST) {
+		return as_error_set(err, AEROSPIKE_ERR_PARAM, "invalid parameter. "
+				"not llist type");
+	}
+
+	/* stack allocate the arg list */
+	as_string ldt_bin;
+	as_string_init(&ldt_bin, (char *)ldt->name, false);
+
+	as_arraylist arglist;
+	as_arraylist_inita(&arglist, 2);
+	as_arraylist_append_string(&arglist, &ldt_bin);
+	as_arraylist_append(&arglist, (as_val *)val);
+
+	as_val* p_return_val = NULL;
+    aerospike_key_apply(
+		as, err, policy, key, ldt->module, operation,
+		(as_list *)&arglist, &p_return_val);
+
+    if (ldt_parse_error(err) != AEROSPIKE_OK) {
+    	return err->code;
+    }
+
+    if (!p_return_val) {
+		return as_error_set(err, AEROSPIKE_ERR_LDT_INTERNAL,
+				"no value returned from server");
+    }
+    int64_t ival = as_integer_getorelse(as_integer_fromval(p_return_val), -1);
+	as_val_destroy(p_return_val);
+
+    if (ival == -1) {
+		return as_error_set(err, AEROSPIKE_ERR_LDT_INTERNAL,
+				"value returned from server not parse-able");
+    }
+
+    return err->code;
+}
+
+as_status aerospike_llist_add(
+	aerospike * as, as_error * err, const as_policy_apply * policy,
+	const as_key * key, const as_ldt * ldt, const as_val * val) {
+	return aerospike_llist_add_internal (as, err, policy, key, ldt, val, LDT_LIST_OP_ADD);
+}
+
+as_status aerospike_llist_addall(
+	aerospike * as, as_error * err, const as_policy_apply * policy,
+	const as_key * key, const as_ldt * ldt, const as_list * vals) {
+	return aerospike_llist_add_internal (as, err, policy, key, ldt,
+			(as_val *)vals, LDT_LIST_OP_ADDALL);
+}
+
+as_status aerospike_llist_size(
+	aerospike * as, as_error * err, const as_policy_apply * policy,
+	const as_key * key, const as_ldt * ldt, uint32_t *n)
+{
+	if ( !err ) {
+		return AEROSPIKE_ERR_PARAM;
+	}
+	as_error_reset(err);
+
+	if (!as || !key || !ldt || !n) {
+		return as_error_set(err, AEROSPIKE_ERR_PARAM, "invalid parameter. "
+				"as/key/ldt/n cannot be null");
+	}
+	if (ldt->type != AS_LDT_LLIST) {
+		return as_error_set(err, AEROSPIKE_ERR_PARAM, "invalid parameter. "
+				"not llist type");
+	}
+
+	/* stack allocate the arg list */
+	as_string ldt_bin;
+	as_string_init(&ldt_bin, (char *)ldt->name, false);
+
+	as_arraylist arglist;
+	as_arraylist_inita(&arglist, 1);
+	as_arraylist_append_string(&arglist, &ldt_bin);
+
+	as_val* p_return_val = NULL;
+    aerospike_key_apply(
+		as, err, policy, key, ldt->module, LDT_LIST_OP_SIZE,
+		(as_list *)&arglist, &p_return_val);
+
+    if (ldt_parse_error(err) != AEROSPIKE_OK) {
+    	return err->code;
+    }
+
+    if (!p_return_val) {
+		return as_error_set(err, AEROSPIKE_ERR_LDT_INTERNAL,
+				"no value returned from server");
+    }
+    int64_t ival = as_integer_getorelse(as_integer_fromval(p_return_val), -1);
+	as_val_destroy(p_return_val);
+
+    if (ival == -1) {
+		return as_error_set(err, AEROSPIKE_ERR_LDT_INTERNAL,
+				"value returned from server not parse-able");
+    }
+    *n = ival;
+
+    return err->code;
+}
+
+as_status aerospike_llist_exists(
+	aerospike * as, as_error * err, const as_policy_apply * policy,
+	const as_key * key, const as_ldt * ldt, const as_val * val, as_boolean *exists)
+{
+	if ( !err ) {
+		return AEROSPIKE_ERR_PARAM;
+	}
+	as_error_reset(err);
+
+	if (!as || !key || !ldt || !exists) {
+		return as_error_set(err, AEROSPIKE_ERR_PARAM, "invalid parameter. "
+				"as/key/ldt/n cannot be null");
+	}
+	if (ldt->type != AS_LDT_LLIST) {
+		return as_error_set(err, AEROSPIKE_ERR_PARAM, "invalid parameter. "
+				"not llist type");
+	}
+
+	/* stack allocate the arg list */
+	as_string ldt_bin;
+	as_string_init(&ldt_bin, (char *)ldt->name, false);
+
+	as_arraylist arglist;
+	as_arraylist_inita(&arglist, 2);
+	as_arraylist_append_string(&arglist, &ldt_bin);
+	as_arraylist_append(&arglist, (as_val *)val);
+
+	as_val* p_return_val = NULL;
+    aerospike_key_apply(
+		as, err, policy, key, ldt->module, LDT_LIST_OP_EXISTS,
+		(as_list *)&arglist, &p_return_val);
+
+    if (ldt_parse_error(err) != AEROSPIKE_OK) {
+    	return err->code;
+    }
+
+    if (!p_return_val) {
+		return as_error_set(err, AEROSPIKE_ERR_LDT_INTERNAL,
+				"no value returned from server");
+    }
+    int64_t ival = as_integer_getorelse(as_integer_fromval(p_return_val), -1);
+	as_val_destroy(p_return_val);
+
+    if (ival == -1) {
+		return as_error_set(err, AEROSPIKE_ERR_LDT_INTERNAL,
+				"value returned from server not parse-able");
+    }
+
+    return err->code;
+} // aerospike_llist_exists()
+
+
+as_status aerospike_llist_filter(
+	aerospike * as, as_error * err, const as_policy_apply * policy,
+	const as_key * key, const as_ldt * ldt,
+	const as_udf_function_name filter, const as_list *filter_args,
+	as_list ** elements )
+{
+	if ( !err ) {
+		return AEROSPIKE_ERR_PARAM;
+	}
+	as_error_reset(err);
+
+	if (filter_args && !filter) {
+		return as_error_set(err, AEROSPIKE_ERR_PARAM, "invalid parameter. "
+				"filter arguments without filter name specification");
+	}
+	if (!as || !key || !ldt || !elements) {
+		return as_error_set(err, AEROSPIKE_ERR_PARAM, "invalid parameter. "
+				"as/key/ldt/peek_count/elements cannot be null");
+	}
+	if (ldt->type != AS_LDT_LLIST) {
+		return as_error_set(err, AEROSPIKE_ERR_PARAM, "invalid parameter. "
+				"not llist type");
+	}
+
+	int list_argc = filter ? 3 : 1;
+	/* stack allocate the arg list */
+	as_string ldt_bin;
+	as_string_init(&ldt_bin, (char *)ldt->name, false);
+
+	as_arraylist arglist;
+	as_arraylist_inita(&arglist, list_argc);
+	as_arraylist_append_string(&arglist, &ldt_bin);
+
+    if (filter){
+    	as_string filter_name;
+    	as_string_init(&filter_name, (char *)filter, false);
+        as_arraylist_append_string(&arglist, &filter_name );
+        as_val_reserve( filter_args ); // bump the ref count
+        as_arraylist_append(&arglist, (as_val *) filter_args );
+    }
+
+	as_val* p_return_val = NULL;
+    aerospike_key_apply(
+		as, err, policy, key, ldt->module, filter ? LDT_LIST_OP_SCAN_WITH_FILTER : LDT_LIST_OP_SCAN,
+		(as_list *)&arglist, &p_return_val);
+
+    if (ldt_parse_error(err) != AEROSPIKE_OK) {
+    	return err->code;
+    }
+
+    if (!p_return_val) {
+		return as_error_set(err, AEROSPIKE_ERR_LDT_INTERNAL,
+				"no value returned from server");
+    }
+
+    *elements = (as_list *)p_return_val;
+
+    return err->code;
+
+} // aerospike_llist_scan()
+
+
+as_status aerospike_llist_remove(
+	aerospike * as, as_error * err, const as_policy_apply * policy,
+	const as_key * key, const as_ldt * ldt, const as_val *val
+	)
+{
+	if ( !err ) {
+		return AEROSPIKE_ERR_PARAM;
+	}
+	as_error_reset(err);
+
+	if (!as || !key || !ldt) {
+		return as_error_set(err, AEROSPIKE_ERR_PARAM, "invalid parameter. "
+				"as/key/ldt/capacity cannot be null");
+	}
+	if (ldt->type != AS_LDT_LLIST) {
+		return as_error_set(err, AEROSPIKE_ERR_PARAM, "invalid parameter. "
+				"not llist type");
+	}
+
+	/* stack allocate the arg list */
+	as_string ldt_bin;
+	as_string_init(&ldt_bin, (char *)ldt->name, false);
+
+	as_arraylist arglist;
+	as_arraylist_inita(&arglist, 2);
+	as_arraylist_append_string(&arglist, &ldt_bin);
+    as_val_reserve( val );
+	as_arraylist_append(&arglist, (as_val *) val);
+
+	as_val* p_return_val = NULL;
+    aerospike_key_apply(
+		as, err, policy, key, ldt->module, LDT_LIST_OP_REMOVE,
+		(as_list *)&arglist, &p_return_val);
+
+    if (ldt_parse_error(err) != AEROSPIKE_OK) {
+    	return err->code;
+    }
+
+    if (p_return_val != NULL) {
+    	as_val_destroy(p_return_val);
+    }
+
+    return err->code;
+}
+
+
+as_status aerospike_llist_destroy(
+	aerospike * as, as_error * err, const as_policy_apply * policy,
+	const as_key * key, const as_ldt * ldt
+	)
+{
+	if ( !err ) {
+		return AEROSPIKE_ERR_PARAM;
+	}
+	as_error_reset(err);
+
+	if (!as || !key || !ldt) {
+		return as_error_set(err, AEROSPIKE_ERR_PARAM, "invalid parameter. "
+				"as/key/ldt/capacity cannot be null");
+	}
+	if (ldt->type != AS_LDT_LLIST) {
+		return as_error_set(err, AEROSPIKE_ERR_PARAM, "invalid parameter. "
+				"not llist type");
+	}
+
+	/* stack allocate the arg list */
+	as_string ldt_bin;
+	as_string_init(&ldt_bin, (char *)ldt->name, false);
+
+	as_arraylist arglist;
+	as_arraylist_inita(&arglist, 1);
+	as_arraylist_append_string(&arglist, &ldt_bin);
+
+	as_val* p_return_val = NULL;
+    aerospike_key_apply(
+		as, err, policy, key, ldt->module, LDT_LIST_OP_DESTROY,
+		(as_list *)&arglist, &p_return_val);
+
+    if (ldt_parse_error(err) != AEROSPIKE_OK) {
+    	return err->code;
+    }
+
+    int64_t ival = as_integer_getorelse(as_integer_fromval(p_return_val), -1);
+	as_val_destroy(p_return_val);
+
+    if (ival != 0) {
+		return as_error_set(err, AEROSPIKE_ERR_LDT_INTERNAL,
+				"value returned from server not parse-able");
+    }
+
+    return err->code;
+}

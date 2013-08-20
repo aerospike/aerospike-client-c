eca6dbc8f1b3c85c3e76788f28292d3b4af21863
Gayathri  Kaliyamoorthy <Gayathri@localhost.localdomain>
2013-08-20 21:22:17 +0530

    Implemented aerospike_batch_get functionality.

--- aerospike/src/main/aerospike/aerospike_batch.c
+++ aerospike/src/main/aerospike/aerospike_batch.c
@@ -31,6 +31,64 @@
 #include <aerospike/as_status.h>
 #include <aerospike/as_val.h>
 
+#include <citrusleaf/cl_batch.h>
+/************************************************************************
+ * 	STRUCTURES
+ ************************************************************************/
+
+typedef struct batch_bridge_s{
+	
+	//user-provided data
+	void *udata;
+
+	//user-provided callback for batch read
+	aerospike_batch_read_callback read_cb;
+	
+	//user-provided callback for batch exist
+	aerospike_batch_exists_callback exist_cb;
+
+	//to distinguish between read and exists callbacks
+	bool is_read;
+} batch_bridge;
+
+/**************************************************************************
+ * 	STATIC FUNCTIONS
+ **************************************************************************/
+
+static int simplebatch_cb(
+	char *ns, cf_digest *keyd, char *set, uint32_t generation,
+	uint32_t record_void_time, cl_bin *bins, int n_bins, bool is_last, void *udata)
+{
+	batch_bridge * bridge = (batch_bridge * ) udata;
+	
+	// Fill the bin data
+	as_record _rec, * rec = &_rec;
+    as_record_inita(rec, n_bins);
+    clbins_to_asrecord(bins, n_bins, rec); 	
+
+	// Fill the metadata
+	 as_key_init_value(&rec->key, ns, set, NULL);
+     memcpy(rec->key.digest.value, keyd, sizeof(cf_digest));
+     rec->key.digest.init = true;
+     rec->gen = generation;
+     rec->ttl = record_void_time;
+	
+	// Call the callback that user wanted to callback
+	if( bridge->is_read == true) {
+		 bridge->read_cb(&rec->key, rec, bridge->udata);
+	}
+	// The responsibility to free the bins is on the called callback function
+	citrusleaf_bins_free(bins, n_bins);
+
+	// release the record
+	as_record_destroy(rec);
+
+    return 0;
+}
+
+
+ 	
+
 /******************************************************************************
  *	FUNCTIONS
  *****************************************************************************/
@@ -44,7 +102,29 @@ as_status aerospike_batch_get(
 	aerospike_batch_read_callback callback, void * udata
 	)
 {
-	return AEROSPIKE_OK;
+	as_status rc = AEROSPIKE_OK;
+
+	batch_bridge bridge_udata; 
+	bridge_udata.udata = udata;
+	bridge_udata.read_cb = callback;
+
+	int n_digests = batch->keys.size;
+	cf_digest * digests = NULL;
+
+	if (n_digests > 0) {
+		digests = (cf_digest *) malloc (sizeof(cf_digest) * n_digests);
+		for ( int i=0; i<n_digests; i++){
+			memcpy(digests[i].digest, batch->keys.entries[i].digest.value, sizeof(cf_digest));
+		}
+	}		
+	
+	
+	cl_rv rv = citrusleaf_get_many_digest(as->cluster, batch->keys.entries[0].ns, digests, n_digests, NULL, 0, 
+					batch->get_key, simplebatch_cb, &bridge_udata);
+		
+	rc = as_error_fromrc(err, rv);
+
+	return rc;
 }
 
 /**

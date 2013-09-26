e16fb7e0fb421322d9396e2edfdac5f43c5d10be
AvinashPatil <avinash@citrusleaf.com>
2013-09-26 21:58:59 +0530

    AER-1246 :  Modify the loop_udf for longevity tests. Added list, map and hybrid of list and map as values in insert and validate function.

--- aerospike/examples/udf_examples/loop_udf/src/lua/udf_loop_test.lua
+++ aerospike/examples/udf_examples/loop_udf/src/lua/udf_loop_test.lua
@@ -16,10 +16,17 @@ function do_loop_test(r, k, v)
 end
 
 function insert(r, k, v)
+-- k is bin_name 
+-- v is 'key' stored as bin data 
+
 --  info("+:  %s=%s", k or '<null>', v or '<null>' )
     if k then
 --      info("r: %s=%s", k or '<null>', (r[k] or '<null>') )
         if v then
+
+	    r['mylist'] = list {v+1, v+2, v+3}
+	    r['mymap'] = map {a=v+1, b=v+2, c=v+3}
+	    r['hybrid'] = list{map{a=v+1}, map{b=v+2}}
             r[k] = v
 --          info("w: %s=%s", k or '<null>', (r[k] or '<null>') )
             if not aerospike:exists(r) then
@@ -28,9 +35,9 @@ function insert(r, k, v)
 	    else
 		return 0
             end
-	end   
+	end
     end
-    return -1 
+    return -1
 end
 
 function remove(r, k, v)
@@ -61,20 +68,34 @@ function update(r, k, v)
 end
 
 function validate(r, k, v)
+
+-- note that r[k] has key stored as bin data
+
 --  info("+:  %s=%s", k or '<null>', v or '<null>' )
     if k then
 --      info("r: %s=%s", k or '<null>', (r[k] or '<null>') )
-       if v then
             if aerospike:exists(r) then
-		if r[k] ~= v then
-		    return 0
-		else
-		    return 1
-		end
-            end
-	end
+		    
+		    local mylist = r['mylist']
+		    local mymap = r['mymap']
+		    local hybrid = r['hybrid']
+
+		    if mylist[1] ~= r[k]+1 or mylist[2] ~= r[k]+2 or mylist[3] ~= r[k] +3 then
+			    return 0
+		    end
+
+		    if mymap['a'] ~= r[k]+1 or mymap['b'] ~= r[k]+2 or mymap['c'] ~= r[k]+3 then
+			    return 0
+		    end
+
+		   if hybrid[1]['a'] ~= r[k]+1 or hybrid[2]['b'] ~= r[k]+2 then
+			    return 0
+		   end
+
+		   return 1
+	 end
     end
-    return -1
+  return -1
 end
 
 function do_noop_loop()

fe5a0cb26eebc18fe1bf3c67c686c73be0726cfb
Russ Sullivan <russ@citrusleaf.com>
2014-02-03 12:47:11 -0800

    updated, random ASK

--- cl_c/udf_examples/lso_udf/Lso_TestDifferentPeekSizes.sh
+++ cl_c/udf_examples/lso_udf/Lso_TestDifferentPeekSizes.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export UFILE="LsoStoneman"
+
 URLID=1000
 URLID_MOD=9
 CREATED=1400000000
@@ -8,7 +10,7 @@ METH_B=7000000
 STATUS=1
 
 USERID=100
-for NUM_PUSHES in 10 20 50 100 150 200 250 300 400 500 600 700 800 1000 2000; do
+for NUM_PUSHES in 10 20 50 100 150 200 250 300 400 500 600 700 800 1000 2000 10000; do
   I=1;
   while [ $I -le $NUM_PUSHES ]; do
     TUPLE="[${URLID},${CREATED},${METH_A},${METH_B},${STATUS}]"
@@ -20,15 +22,15 @@ for NUM_PUSHES in 10 20 50 100 150 200 250 300 400 500 600 700 800 1000 2000; do
     METH_B=$[${METH_B}+1];
     STATUS=$[${STATUS}+1];
   done
-  for SIZE in 10 20 50 100 150 200 250 300 400 500 600 700 800 1000 2000; do
-    ASK=$[${SIZE}-2]
-    if [ $SIZE -gt $NUM_PUSHES ]; then ASK=0; fi
-    RES=$(ascli udf-record-apply $UNS $USET $USERID $UFILE stackPeek $LSOBIN $ASK | tr \] \\n | wc -l)
-    if [ $ASK -eq 0 ]; then
-      echo "PEEK: $SIZE GOT: $RES  DIFF: " $[${RES}-${NUM_PUSHES}]
-    else
-      echo "PEEK: $SIZE GOT: $RES  DIFF: " $[${RES}-${SIZE}]
-    fi
+  for SIZE in 10 20 50 100 150 200 250 300 400 500 600 700 800 1000 2000 10000; do
+    RAN=$(lua -e "math.randomseed(os.time());print(math.random(10))");
+    ASK=$[${SIZE}-${RAN}]
+    if [ $ASK -gt $NUM_PUSHES ]; then ASK=0; fi
+    RES=$(ascli udf-record-apply $UNS $USET $USERID $UFILE stackPeek $LSOBIN $ASK | tr \] \\n | grep \\[ | wc -l)
+    EXPECT=$ASK
+    if [ $ASK -eq 0 ]; then EXPECT=$NUM_PUSHES; fi
+    DIFF=$[${RES}-${EXPECT}];
+    echo "SIZE: $NUM_PUSHES USERID: $USERID [PEEK: $EXPECT GOT: $RES  DIFF: $DIFF"
     if [ $SIZE -gt $NUM_PUSHES ]; then break; fi
   done
   USERID=$[${USERID}+1];

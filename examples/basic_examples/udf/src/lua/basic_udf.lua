632e748f6055fe796c3fc54ddbb5c8e8ff78ff37
Andrew Gooding <andy@aerospike.com>
2014-07-16 14:09:13 -0700

    Changed lua variable names from 'record' to 'rec' to reduce confusion, since 'record' is a reserved word.

--- examples/basic_examples/udf/src/lua/basic_udf.lua
+++ examples/basic_examples/udf/src/lua/basic_udf.lua
@@ -1,16 +1,16 @@
 -- A very simple arithmetic UDF with no arguments or return value.
 -- It adds 1000 to the value in bin 'test-bin-1' and updates it.
-function test_bin_1_add_1000(record)
-	record['test-bin-1'] = record['test-bin-1'] + 1000
-	aerospike:update(record)
+function test_bin_1_add_1000(rec)
+	rec['test-bin-1'] = rec['test-bin-1'] + 1000
+	aerospike:update(rec)
 end
 
 -- A simple arithmetic UDF that has arguments and a return value.
 -- It updates the value in the specified bin after performing the arithmetic
 -- operation, and returns the resulting bin value.
-function bin_transform(record, bin_name, x, y)
-	record[bin_name] = (record[bin_name] * x) + y
-	aerospike:update(record)
-	return record[bin_name]
+function bin_transform(rec, bin_name, x, y)
+	rec[bin_name] = (rec[bin_name] * x) + y
+	aerospike:update(rec)
+	return rec[bin_name]
 end
 

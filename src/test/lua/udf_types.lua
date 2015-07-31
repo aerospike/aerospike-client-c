f2e38d02edaaac77bb44bd7da7c337a88b48d6ac
Brad Whisler <brad@aerospike.com>
2015-07-31 18:01:41 +0530

    Add as_bytes tests to the udf_types test suite.

--- src/test/lua/udf_types.lua
+++ src/test/lua/udf_types.lua
@@ -30,14 +30,28 @@ function get_list()
     return list {1,2,3}
 end
 
+function get_bytes()
+	local b = bytes(3)
+	bytes.set_string(b, 1, 'zyx')
+	return b
+end
+
+function modify_bytes(rec, b)
+	bytes.set_byte(b, 2, 135)
+	return b
+end
+
 function get_rec_map(rec)
     local m = map()
+	local b = bytes(3)
     m.t = true
     m.f = false
     m.n = nil
     m.i = 123
     m.s = "abc"
     m.l = list{1,2,3}
+	bytes.set_string(b, 1, 'zyx')
+	m.b = b
     rec['map'] = m
     return rec['map']
 end

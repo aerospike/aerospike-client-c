50f5b5a41da9e3c2096d4f60ed86aed2686122d9
Thomas Lopatic <thomas@aerospike.com>
2015-11-05 16:01:59 +0100

    Keep track of pending commands.

--- src/main/aerospike/as_node.c
+++ src/main/aerospike/as_node.c
@@ -83,6 +83,7 @@ as_node_create(as_cluster* cluster, struct sockaddr_in* addr, as_node_info* node
 		node->pipe_conn_qs = 0;
 	}
 	
+	node->async_pending = 0;
 	node->info_fd = -1;
 	node->friends = 0;
 	node->failures = 0;

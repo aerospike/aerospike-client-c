e4d2f1e2c8dc54b03bd9c83fa9345bc5199024dc
Shu Chen <shu@aerospike.com>
2015-12-11 17:41:34 -0800

    Support variable args for pop_range/remove_range. Add more test cases to cdt list test.

--- src/test/aerospike_cdt/cdt_basics.c
+++ src/test/aerospike_cdt/cdt_basics.c
@@ -62,6 +62,8 @@ typedef struct as_testlist_s {
  * STATIC FUNCTIONS
  *****************************************************************************/
 
+static bool as_testlist_compare(as_testlist *tlist);
+
 static bool
 has_cdt_list()
 {
@@ -80,12 +82,51 @@ has_cdt_list()
 	return false;
 }
 
+static bool
+as_val_is_equal(const as_val *v0, const as_val *v1)
+{
+	as_val_t type0 = as_val_type(v0);
+	if (type0 == as_val_type(v1)) {
+		switch (type0) {
+		case AS_INTEGER:
+			return as_integer_get((as_integer *)v0) == as_integer_get((as_integer *)v1);
+		case AS_STRING:
+			return strcmp(as_string_get((as_string *)v0), as_string_get((as_string *)v1)) == 0;
+		case AS_NIL:
+			return true;
+		default:
+			error("Type %d not supported for is_equal.", type0);
+			return false;
+		}
+	}
+	else if (type0 == AS_NIL) {
+		return v1 == NULL;
+	}
+	else if (as_val_type(v1) == AS_NIL) {
+		return v0 == NULL;
+	}
+
+	return false;
+}
+
 void
 make_random_list(as_arraylist *list, uint32_t count)
 {
 	as_arraylist_init(list, count, 1);
 	for (uint32_t i = 0; i < count; i++) {
-		as_arraylist_append(list, (as_val *)as_integer_new(rand()%1000));
+		if (rand() % 2 == 0) {
+			as_arraylist_append(list, (as_val *)as_integer_new(rand()%1000));
+		}
+		else {
+			int len = rand() % 100;
+			char str[len + 1];
+			for (int i = 0; i < len; i++) {
+				// Random chars from 32 (space) to 126 (~)
+				str[i] = (rand() % (126 - 32 + 1)) + 32;
+			}
+			str[len] = '\0';
+			as_arraylist_append(list, (as_val *)as_string_new_strdup(str));
+		}
 	}
 }
 
@@ -100,7 +141,7 @@ as_testlist_op(as_testlist *tlist, as_operations *ops)
     as_error err;
 
 	if (aerospike_key_operate(tlist->as, &err, NULL, &tlist->key, ops, &tlist->rec) != AEROSPIKE_OK) {
-		info("as_testlist_op() returned %d - %s", err.code, err.message);
+		debug("as_testlist_op() returned %d - %s", err.code, err.message);
 		return false;
 	}
 
@@ -109,12 +150,15 @@ as_testlist_op(as_testlist *tlist, as_operations *ops)
 	return true;
 }
 
-static void
+static bool
 as_testlist_init(as_testlist *tlist, aerospike *as)
 {
 	tlist->as = as;
 	tlist->rec = NULL;
 	as_key_init_int64(&tlist->key, NAMESPACE, SET, 1);
+
+    as_error err;
+    aerospike_key_remove(as, &err, NULL, &tlist->key);
 
 	as_arraylist_init(&tlist->arraylist, 100, 100);
 
@@ -126,7 +170,38 @@ as_testlist_init(as_testlist *tlist, aerospike *as)
 
 	as_operations_add_write(&ops, BIN_NAME, (as_bin_value *)&list);
 
-	as_testlist_op(tlist, &ops);
+	if (! as_testlist_op(tlist, &ops)) {
+		error("write empty list failed");
+		return false;
+	}
+
+	debug("wrote empty list");
+	if (! as_testlist_compare(tlist)) {
+		error("write empty list: post compare failed");
+		return false;
+	}
+
+	debug("remove record");
+	if (aerospike_key_remove(as, &err, NULL, &tlist->key) != AEROSPIKE_OK) {
+		error("aerospike_key_remove failed");
+		return false;
+	}
+
+	as_operations_inita(&ops, 1);
+	as_operations_add_list_append_items(&ops, BIN_NAME, &list);
+
+	debug("append empty list");
+	if (! as_testlist_op(tlist, &ops)) {
+		error("append empty list failed");
+		return false;
+	}
+
+	if (! as_testlist_compare(tlist)) {
+		error("append empty list: post compare failed");
+		return false;
+	}
+
+	return true;
 }
 
 static void
@@ -142,13 +217,13 @@ as_testlist_destroy(as_testlist *tlist)
 	}
 }
 
-static uint32_t
-index2uindex(as_testlist *tlist, int index)
+static uint64_t
+index2uindex(as_testlist *tlist, int64_t index)
 {
 	if (index < 0) {
 		return as_arraylist_size(&tlist->arraylist) + index;
 	}
-	return (uint32_t)index;
+	return (uint64_t)index;
 }
 
 static bool
@@ -157,7 +232,7 @@ as_testlist_remove(as_testlist *tlist, int index, bool is_pop)
 	as_operations ops;
 	as_operations_inita(&ops, 1);
 
-	uint32_t uindex = index2uindex(tlist, index);
+	uint64_t uindex = index2uindex(tlist, index);
 
 	as_arraylist_remove(&tlist->arraylist, uindex);
 	if (is_pop) {
@@ -176,11 +251,16 @@ as_testlist_remove_range(as_testlist *tlist, int index, uint32_t count, bool is_
 	as_operations ops;
 	as_operations_inita(&ops, 1);
 
-	uint32_t uindex = index2uindex(tlist, index);
+	uint64_t uindex = index2uindex(tlist, index);
 
+	bool first_index_invalid = false;
 	for (uint32_t i = 0; i < count; i++) {
-		as_arraylist_remove(&tlist->arraylist, uindex);
+		int ret = as_arraylist_remove(&tlist->arraylist, uindex);
+		if (ret != AS_ARRAYLIST_OK && i == 0) {
+			first_index_invalid = true;
+		}
 	}
+
 	if (is_pop) {
 		as_operations_add_list_pop_range(&ops, BIN_NAME, (int64_t)index, (uint64_t)count);
 	}
@@ -188,8 +268,96 @@ as_testlist_remove_range(as_testlist *tlist, int index, uint32_t count, bool is_
 		as_operations_add_list_remove_range(&ops, BIN_NAME, (int64_t)index, (uint64_t)count);
 	}
 
-	return as_testlist_op(tlist, &ops);
+	bool ret = as_testlist_op(tlist, &ops);
+
+	if (! ret) {
+		if (first_index_invalid) {
+			debug("remove_range: index=%d count=%d out_of_range=%s failed as expected", index, count, first_index_invalid ? "true" : "false");
+			return true;
+		}
+
+		debug("remove_range: index=%d count=%d out_of_range=%s", index, count, first_index_invalid ? "true" : "false");
+		return false;
+	}
+
+	debug("remove_range: index=%d count=%d out_of_range=%s", index, count, first_index_invalid ? "true" : "false")
+	return true;
 }
+
+static bool
+compare_range(as_arraylist *a, uint32_t index, as_list *list)
+{
+	uint32_t result_size = as_list_size(list);
+
+	for (uint32_t i = 0; i < result_size; i++) {
+		uint32_t test_index = index + i;
+		as_val *v0 = as_arraylist_get(a, test_index);
+		as_val *v1 = as_list_get(list, i);
+		if (! as_val_is_equal(v0, v1)) {
+			char *s0 = as_val_tostring(v0);
+			char *s1 = as_val_tostring(v1);
+			debug("compare_range: index=%u: %s != %s", i, s0, s1);
+			free(s0);
+			free(s1);
+			return false;
+		}
+	}
+
+	return true;
+}
+
+static bool
+as_testlist_get_range(as_testlist *tlist, int64_t index, uint32_t count)
+{
+	as_operations ops;
+	as_operations_inita(&ops, 1);
+
+	as_operations_add_list_get_range(&ops, BIN_NAME, index, count);
+
+	bool ret = as_testlist_op(tlist, &ops);
+	if (! ret) {
+		return false;
+	}
+
+	uint64_t uindex = index2uindex(tlist, index);
+	as_list *list = as_record_get_list(tlist->rec, BIN_NAME);
+	uint32_t result_size = as_list_size(list);
+
+	debug("get_range: result_size=%u", result_size);
+	return compare_range(&tlist->arraylist, uindex, list);
+}
+
+static bool
+as_testlist_get_range_from(as_testlist *tlist, int64_t index)
+{
+	as_operations ops;
+	as_operations_inita(&ops, 1);
+
+	as_operations_add_list_get_range_from(&ops, BIN_NAME, index);
+
+	bool ret = as_testlist_op(tlist, &ops);
+	if (! ret) {
+		if (index > as_arraylist_size(&tlist->arraylist)) {
+			debug("get_range_from: index=%ld failed as expected", index);
+			return false;
+		}
+
+		debug("get_range_from: index=%ld failed", index);
+		return false;
+	}
+
+	uint64_t uindex = index2uindex(tlist, index);
+	as_list *list = as_record_get_list(tlist->rec, BIN_NAME);
+	uint32_t result_size = as_list_size(list);
+
+	if (uindex + result_size != as_arraylist_size(&tlist->arraylist)) {
+		return false;
+	}
+
+	debug("get_range_from: result_size=%u", result_size);
+	return compare_range(&tlist->arraylist, uindex, list);
+}
+
 
 static bool
 as_testlist_append(as_testlist *tlist, as_val *val)
@@ -222,7 +390,7 @@ as_testlist_insert(as_testlist *tlist, int64_t index, as_val *val)
 	as_operations ops;
 	as_operations_inita(&ops, 1);
 
-	uint32_t uindex = index2uindex(tlist, index);
+	uint64_t uindex = index2uindex(tlist, index);
 	as_val_reserve(val);
 	as_arraylist_insert(&tlist->arraylist, uindex, val);
 
@@ -237,7 +405,7 @@ as_testlist_insert_list(as_testlist *tlist, int64_t index, as_arraylist *list)
 	as_operations ops;
 	as_operations_inita(&ops, 1);
 
-	uint32_t uindex = index2uindex(tlist, index);
+	uint64_t uindex = index2uindex(tlist, index);
 	uint32_t size = as_arraylist_size(list);
 	for (int i = size - 1; i >= 0; i--) {
 		as_val *val = as_arraylist_get(list, i);
@@ -256,7 +424,7 @@ as_testlist_set(as_testlist *tlist, int64_t index, as_val *val)
 	as_operations ops;
 	as_operations_inita(&ops, 1);
 
-	uint32_t uindex = index2uindex(tlist, index);
+	uint64_t uindex = index2uindex(tlist, index);
 	as_val_reserve(val);
 	as_arraylist_set(&tlist->arraylist, uindex, val);
 
@@ -271,7 +439,7 @@ as_testlist_trim(as_testlist *tlist, int64_t index, uint64_t count)
 	as_operations ops;
 	as_operations_inita(&ops, 1);
 
-	uint32_t uindex = index2uindex(tlist, index);
+	uint64_t uindex = index2uindex(tlist, index);
 	as_arraylist_trim(&tlist->arraylist, uindex + count);
 	for (int64_t i = 0; i < index; i++) {
 		as_arraylist_remove(&tlist->arraylist, 0);
@@ -315,16 +483,22 @@ as_testlist_compare(as_testlist *tlist)
 	uint32_t local_size = as_arraylist_size(&tlist->arraylist);
 
 	if (server_size != local_size) {
-		info("as_testlist_compare() server_size: %u local_size: %u", server_size, local_size);
+		debug("as_testlist_compare() server_size: %u != local_size: %u", server_size, local_size);
+		char *s = as_val_tostring(list);
+		debug("as_testlist_compare() server_list = %s", s);
+		free(s);
 		return false;
 	}
 
-	for (int i = 0; i < local_size; i++) {
-		int64_t server_value = as_list_get_int64(list, i);
-		int64_t local_value = as_arraylist_get_int64(&tlist->arraylist, i);
-
-		if (server_value != local_value) {
-			info("as_testlist_compare() server_value: %u local_value: %u", server_value, local_value);
+	for (uint32_t i = 0; i < local_size; i++) {
+		as_val *v0 = as_arraylist_get(&tlist->arraylist, i);
+		as_val *v1 = as_list_get(list, i);
+		if (! as_val_is_equal(v0, v1)) {
+			char *s0 = as_val_tostring(v0);
+			char *s1 = as_val_tostring(v1);
+			debug("as_testlist_compare() at index: %u server_value: %s != local_value: %s", i, s1, s0);
+			free(s0);
+			free(s1);
 			return false;
 		}
 	}
@@ -344,7 +518,7 @@ TEST( cdt_basics_op , "CDT operations test on a single bin" ) {
 	}
 
 	as_testlist tlist;
-	as_testlist_init(&tlist, as);
+	assert_true( as_testlist_init(&tlist, as) );
 
 	debug("insert 1");
 	for (int i = 0; i < 100; i++) {
@@ -414,6 +588,21 @@ TEST( cdt_basics_op , "CDT operations test on a single bin" ) {
 		assert_true( as_testlist_remove_range(&tlist, ridx, 5, false) );
 	}
 	assert_true( as_testlist_compare(&tlist) );
+
+	debug("remove_range: negative out of range");
+	assert_true( as_testlist_remove_range(&tlist, -100, 5, false) );
+
+	debug("remove_range: negative index");
+	assert_true( as_testlist_remove_range(&tlist, -1, 5, false) );
+
+	debug("remove_range: 0 count");
+	assert_true( as_testlist_remove_range(&tlist, 1, 0, false) );
+
+	debug("get_range");
+	as_testlist_get_range(&tlist, 0, 22);
+	as_testlist_get_range(&tlist, 10, 22);
+	as_testlist_get_range_from(&tlist, 20);
+	as_testlist_get_range_from(&tlist, 25);
 
 	debug("set");
 	for (int i = 0; i < 100; i++) {

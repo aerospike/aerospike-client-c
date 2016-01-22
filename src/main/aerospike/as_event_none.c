49f19759f28bc25d0c8d989ab5fb5f67920e6bef
Brian Nichols <bnichols@aerospike.com>
2016-01-22 11:33:05 -0800

    Add new as_event_none stubs.

--- src/main/aerospike/as_event_none.c
+++ src/main/aerospike/as_event_none.c
@@ -56,9 +56,14 @@ as_event_node_destroy(as_node* node)
 }
 
 bool
-as_event_close_loop(as_event_loop* event_loop)
+as_event_send_close_loop(as_event_loop* event_loop)
 {
 	return false;
+}
+
+void
+as_event_close_loop(as_event_loop* event_loop)
+{
 }
 
 #endif

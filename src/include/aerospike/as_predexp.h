94e63b818eae45af6d8fbbc3064bbc9c984ea626
Ken Sedgwick <ken@bonsai.com>
2017-02-27 18:01:55 -0800

    predexp: Improved header documentation regarding predexp assembly.

--- src/include/aerospike/as_predexp.h
+++ src/include/aerospike/as_predexp.h
@@ -49,10 +49,14 @@ typedef struct as_predexp_base_s {
 } as_predexp_base;
 
 /**
- *	Create a logical AND predicate expression.
+ *	Create an AND logical predicate expression.
  *
  *  The AND predicate expression returns true if all of it's children
  *  are true.
+ *
+ *  The nexpr parameter specifies how many childen to pop off the
+ *  expression stack.  These children must be "logical" expressions
+ *  and not "value" expressions.
  *
  *  For example, the following sequence of predicate expressions
  *  selects records where the value of bin "c" is between 11 and 20
@@ -78,10 +82,14 @@ as_predexp_base*
 as_predexp_and(uint16_t nexpr);
 	
 /**
- *	Create a logical OR predicate expression.
+ *	Create an OR logical predicate expression.
  *
  *  The OR predicate expression returns true if any of it's children
  *  are true.
+ *
+ *  The nexpr parameter specifies how many children to pop off the
+ *  expression stack.  These children must be "logical" expressions
+ *  and not "value" expressions.
  *
  *  For example, the following sequence of predicate expressions
  *  selects records where the value of bin "pet" is "cat" or "dog".
@@ -106,9 +114,13 @@ as_predexp_base*
 as_predexp_or(uint16_t nexpr);
 	
 /**
- *	Create a logical NOT predicate expression.
+ *	Create a NOT logical predicate expression.
  *
  *  The NOT predicate expression returns true if it's child is false.
+ *
+ *  The NOT expression pops a single child off the expression stack.
+ *  This child must be a "logical" expression and not a "value"
+ *  expression.
  *
  *  For example, the following sequence of predicate expressions
  *  selects records where the value of bin "pet" is not "dog".
@@ -129,6 +141,9 @@ as_predexp_not();
 
 /**
  *	Create a constant integer value predicate expression.
+ *
+ *  The integer value predicate expression pushes a single constant
+ *  integer value onto the expression stack.
  *
  *  For example, the following sequence of predicate expressions
  *  selects records where the value of bin "c" is between 11 and 20
@@ -156,6 +171,9 @@ as_predexp_integer_value(int64_t value);
 /**
  *	Create a constant string value predicate expression.
  *
+ *  The string value predicate expression pushes a single constant
+ *  string value onto the expression stack.
+ *
  *  For example, the following sequence of predicate expressions
  *  selects records where the value of bin "pet" is "cat" or "dog":
  *
@@ -180,6 +198,9 @@ as_predexp_string_value(char const * value);
 
 /**
  *	Create a constant GeoJSON value predicate expression.
+ *
+ *  The GeoJSON value predicate expression pushes a single constant
+ *  GeoJSON value onto the expression stack.
  *
  *  For example, the following sequence of predicate expressions
  *  selects records where a point in bin "loc" is inside the
@@ -211,7 +232,10 @@ as_predexp_base*
 as_predexp_geojson_value(char const * value);
 
 /**
- *	Create an integer bin predicate expression.
+ *	Create an integer bin value predicate expression.
+ *
+ *  The integer bin predicate expression pushes a single integer bin
+ *  value extractor onto the expression stack.
  *
  *  For example, the following sequence of predicate expressions
  *  selects records where the value of bin "c" is between 11 and 20
@@ -237,7 +261,10 @@ as_predexp_base*
 as_predexp_integer_bin(char const * binname);
 
 /**
- *	Create an string bin predicate expression.
+ *	Create an string bin value predicate expression.
+ *
+ *  The string bin predicate expression pushes a single string bin
+ *  value extractor onto the expression stack.
  *
  *  For example, the following sequence of predicate expressions
  *  selects records where the value of bin "pet" is "cat" or "dog":
@@ -262,7 +289,10 @@ as_predexp_base*
 as_predexp_string_bin(char const * binname);
 
 /**
- *	Create an GeoJSON bin predicate expression.
+ *	Create an GeoJSON bin value predicate expression.
+ *
+ *  The GeoJSON bin predicate expression pushes a single GeoJSON bin
+ *  value extractor onto the expression stack.
  *
  *  For example, the following sequence of predicate expressions
  *  selects records where a point in bin "loc" is inside the
@@ -294,7 +324,12 @@ as_predexp_base*
 as_predexp_geojson_bin(char const * binname);
 
 /**
- *	Create a list bin predicate expression.
+ *	Create a list bin value predicate expression.
+ *
+ *  The list bin predicate expression pushes a single list bin value
+ *  extractor onto the expression stack.  List bin values may be used
+ *  with list iteration expressions to evaluate a subexpression for
+ *  each of the elements of the list.
  *
  *  For example, the following sequence of predicate expressions
  *  selects records where one of the list items is "cat":
@@ -317,7 +352,12 @@ as_predexp_base*
 as_predexp_list_bin(char const * binname);
 
 /**
- *	Create a map bin predicate expression.
+ *	Create a map bin value predicate expression.
+ *
+ *  The map bin predicate expression pushes a single map bin value
+ *  extractor onto the expression stack.  Map bin values may be used
+ *  with map iteration expressions to evaluate a subexpression for
+ *  each of the elements of the map.
  *
  *  For example, the following sequence of predicate expressions
  *  selects records where the map contains a key of "cat":
@@ -340,7 +380,11 @@ as_predexp_base*
 as_predexp_map_bin(char const * binname);
 
 /**
- *	Create an integer iteration variable predicate expression.
+ *	Create an integer iteration variable (value) predicate expression.
+ *
+ *  The integer iteration variable is used in the subexpression child
+ *  of a list or map iterator and takes the value of each element in
+ *  the collection as it is traversed.
  *
  *  For example, the following sequence of predicate expressions
  *  selects records where the list contains a value of 42:
@@ -363,7 +407,11 @@ as_predexp_base*
 as_predexp_integer_var(char const * varname);
 
 /**
- *	Create an string iteration variable predicate expression.
+ *	Create an string iteration variable (value) predicate expression.
+ *
+ *  The string iteration variable is used in the subexpression child
+ *  of a list or map iterator and takes the value of each element in
+ *  the collection as it is traversed.
  *
  *  For example, the following sequence of predicate expressions
  *  selects records where one of the list items is "cat":
@@ -386,7 +434,11 @@ as_predexp_base*
 as_predexp_string_var(char const * varname);
 
 /**
- *	Create an GeoJSON iteration variable predicate expression.
+ *	Create an GeoJSON iteration variable (value) predicate expression.
+ *
+ *  The GeoJSON iteration variable is used in the subexpression child
+ *  of a list or map iterator and takes the value of each element in
+ *  the collection as it is traversed.
  *
  *  @param varname	The name of the iteration variable.
  *
@@ -397,17 +449,19 @@ as_predexp_base*
 as_predexp_geojson_var(char const * varname);
 
 /**
- *	Create a record size metadata predicate expression.
+ *	Create a record device size metadata value predicate expression.
+ *
+ *  The record device size expression assumes the value of the size in
+ *  bytes that the record occupies on device storage.
  *
  *  For example, the following sequence of predicate expressions
- *  selects records that are larger than 65K:
- *  inclusive:
+ *  selects records whoese device storage size is larger than 65K:
  *
  *	~~~~~~~~~~{.c}
  *	as_query_predexp_inita(&q, 3);
  *	as_query_predexp_add(&q, as_predexp_integer_value(65 * 1024));
  *	as_query_predexp_add(&q, as_predexp_rec_device_size());
- *	as_query_predexp_add(&q, as_predexp_integer_greatereq());
+ *	as_query_predexp_add(&q, as_predexp_integer_greater());
  *	~~~~~~~~~~
  *
  *  @returns a predicate expression suitable for adding to a query or
@@ -417,7 +471,11 @@ as_predexp_base*
 as_predexp_rec_device_size();
 
 /**
- *	Create a last update record metadata predicate expression.
+ *	Create a last update record metadata value predicate expression.
+ *
+ *  The record last update expression assumes the value of the number
+ *  of nanoseconds since the unix epoch that the record was last
+ *  updated.
  *
  *  For example, the following sequence of predicate expressions
  *  selects records that have been updated after an timestamp:
@@ -436,7 +494,11 @@ as_predexp_base*
 as_predexp_rec_last_update();
 
 /**
- *	Create a void time record metadata predicate expression.
+ *	Create a void time record metadata value predicate expression.
+ *
+ *  The record void time expression assumes the value of the number of
+ *  nanoseconds since the unix epoch when the record will expire.  The
+ *  special value of 0 means the record will not expire.
  *
  *  For example, the following sequence of predicate expressions
  *  selects records that have void time set to 0 (no expiration):
@@ -455,16 +517,28 @@ as_predexp_base*
 as_predexp_rec_void_time();
 
 /**
- *	Create an integer comparison predicate expression.
+ *	Create an integer comparison logical predicate expression.
+ *
+ *  The integer comparison expressions pop a pair of value expressions
+ *  off the expression stack and compare them.  The deeper of the two
+ *  child expressions (pushed earlier) is considered the right side of
+ *  the expression and the shallower (pushed later) is considered the
+ *  left side.
+ *
+ *  If the value of either of the child expressions is unknown because
+ *  a specified bin does not exist or contains a value of the wrong
+ *  type the result of the comparison is false.  If a true outcome is
+ *  desirable in this situation use the complimentary comparison and
+ *  enclose in a logical NOT.
  *
  *  For example, the following sequence of predicate expressions
- *  selects records that have bin "foo" equal to 42:
+ *  selects records that have bin "foo" greater than 42:
  *
  *	~~~~~~~~~~{.c}
  *  as_query_predexp_inita(&q, 3);
  *	as_query_predexp_add(&q, as_predexp_integer_value(42));
  *	as_query_predexp_add(&q, as_predexp_integer_bin("foo"));
- *	as_query_predexp_add(&q, as_predexp_integer_equal());
+ *	as_query_predexp_add(&q, as_predexp_integer_greater());
  *	~~~~~~~~~~
  *
  *  @returns a predicate expression suitable for adding to a query or
@@ -489,7 +563,19 @@ as_predexp_base*
 as_predexp_integer_lesseq();
 
 /**
- *	Create an string comparison predicate expression.
+ *	Create an string comparison logical predicate expression.
+ *
+ *  The string comparison expressions pop a pair of value expressions
+ *  off the expression stack and compare them.  The deeper of the two
+ *  child expressions (pushed earlier) is considered the right side of
+ *  the expression and the shallower (pushed later) is considered the
+ *  left side.
+ *
+ *  If the value of either of the child expressions is unknown because
+ *  a specified bin does not exist or contains a value of the wrong
+ *  type the result of the comparison is false.  If a true outcome is
+ *  desirable in this situation use the complimentary comparison and
+ *  enclose in a logical NOT.
  *
  *  For example, the following sequence of predicate expressions
  *  selects records that have bin "pet" equal to "cat":
@@ -511,7 +597,18 @@ as_predexp_base*
 as_predexp_string_unequal();
 
 /**
- *	Create an string regular expression predicate expression.
+ *	Create an string regular expression logical predicate expression.
+ *
+ *  The string regex expression pops two children off the expression
+ *  stack and compares a child value expression to a regular
+ *  expression.  The deeper of the two children (pushed earlier) must
+ *  be a string value containing a valid regular expression.  The
+ *  shallower child (pushed later) must contain the string value to be
+ *  matched.
+ *
+ *  If the value of the shallower child is unknown because a specified
+ *  bin does not exist or contains a value of the wrong type the
+ *  result of the regex match is false.
  *
  *  For example, the following sequence of predicate expressions
  *  selects records that have bin "hex" value ending in '1' or '2':
@@ -532,7 +629,18 @@ as_predexp_base*
 as_predexp_string_regex(uint32_t opts);
 
 /**
- *	Create an GeoJSON Points-in-Region predicate expression.
+ *	Create an GeoJSON Points-in-Region logical predicate expression.
+ *
+ *  The Points-in-Region (within) expression pops two children off the
+ *  expression stack and checks to see if a child GeoJSON point is
+ *  inside a specified GeoJSON region.  The deeper of the two children
+ *  (pushed earlier) must be a GeoJSON value containing a region.  The
+ *  shallower child (pushed later) must contain a GeoJSON value
+ *  specifying a point.
+ *
+ *  If the value of the shallower child is unknown because a specified
+ *  bin does not exist or contains a value of the wrong type the
+ *  result of the within expression is false.
  *
  *  For example, the following sequence of predicate expressions
  *  selects records where a point in bin "loc" is inside the
@@ -562,7 +670,18 @@ as_predexp_base*
 as_predexp_geojson_within();
 
 /**
- *	Create an GeoJSON Regions-Containing-Point predicate expression.
+ *	Create an GeoJSON Regions-Containing-Point logical predicate expression.
+ *
+ *  The Regions-Containing-Point (contains) expression pops two
+ *  children off the expression stack and checks to see if a child
+ *  GeoJSON region contains a specified GeoJSON point.  The deeper of
+ *  the two children (pushed earlier) must be a GeoJSON value
+ *  containing a point.  The shallower child (pushed later) must
+ *  contain a GeoJSON value specifying a possibly enclosing region.
+ *
+ *  If the value of the shallower child is unknown because a specified
+ *  bin does not exist or contains a value of the wrong type the
+ *  result of the contains expression is false.
  *
  *  For example, the following sequence of predicate expressions
  *  selects records where a region in bin "loc" is contains the
@@ -588,7 +707,17 @@ as_predexp_base*
 as_predexp_geojson_contains();
 
 /**
- *	Create an list iteration OR predicate expression.
+ *	Create an list iteration OR logical predicate expression.
+ *
+ *  The list iteration expression pops two children off the expression
+ *  stack.  The shallower child (pushed later) must specify a list
+ *  bin.  The deeper child (pushed earlier) must contain a logical
+ *  subexpression containing one or more matching iteration variable
+ *  expressions.  The list iteration traverses the list and repeatedly
+ *  evaluates the subexpression substituting each list element's value
+ *  into the matching iteration variable.  The result of the iteration
+ *  expression is a logical OR of all of the individual element
+ *  evaluations.
  *
  *  For example, the following sequence of predicate expressions
  *  selects records where one of the list items is "cat":
@@ -611,7 +740,17 @@ as_predexp_base*
 as_predexp_list_iterate_or(char const * varname);
 
 /**
- *	Create an list iteration AND predicate expression.
+ *	Create an list iteration AND logical predicate expression.
+ *
+ *  The list iteration expression pops two children off the expression
+ *  stack.  The shallower child (pushed later) must specify a list
+ *  bin.  The deeper child (pushed earlier) must contain a logical
+ *  subexpression containing one or more matching iteration variable
+ *  expressions.  The list iteration traverses the list and repeatedly
+ *  evaluates the subexpression substituting each list element's value
+ *  into the matching iteration variable.  The result of the iteration
+ *  expression is a logical AND of all of the individual element
+ *  evaluations.
  *
  *  For example, the following sequence of predicate expressions
  *  selects records where none of the list items is "cat":
@@ -635,7 +774,17 @@ as_predexp_base*
 as_predexp_list_iterate_and(char const * varname);
 
 /**
- *	Create an map key iteration OR predicate expression.
+ *	Create an map key iteration OR logical predicate expression.
+ *
+ *  The mapkey iteration expression pops two children off the
+ *  expression stack.  The shallower child (pushed later) must specify
+ *  a map bin.  The deeper child (pushed earlier) must contain a
+ *  logical subexpression containing one or more matching iteration
+ *  variable expressions.  The mapkey iteration traverses the map and
+ *  repeatedly evaluates the subexpression substituting each map key
+ *  value into the matching iteration variable.  The result of the
+ *  iteration expression is a logical OR of all of the individual
+ *  element evaluations.
  *
  *  For example, the following sequence of predicate expressions
  *  selects records where one of the map keys is "cat":
@@ -658,7 +807,17 @@ as_predexp_base*
 as_predexp_mapkey_iterate_or(char const * varname);
 
 /**
- *	Create an map key iteration AND predicate expression.
+ *	Create an map key iteration AND logical predicate expression.
+ *
+ *  The mapkey iteration expression pops two children off the
+ *  expression stack.  The shallower child (pushed later) must specify
+ *  a map bin.  The deeper child (pushed earlier) must contain a
+ *  logical subexpression containing one or more matching iteration
+ *  variable expressions.  The mapkey iteration traverses the map and
+ *  repeatedly evaluates the subexpression substituting each map key
+ *  value into the matching iteration variable.  The result of the
+ *  iteration expression is a logical AND of all of the individual
+ *  element evaluations.
  *
  *  For example, the following sequence of predicate expressions
  *  selects records where none of the map keys is "cat":
@@ -682,7 +841,17 @@ as_predexp_base*
 as_predexp_mapkey_iterate_and(char const * varname);
 
 /**
- *	Create an map value iteration OR predicate expression.
+ *	Create an map value iteration OR logical predicate expression.
+ *
+ *  The mapval iteration expression pops two children off the
+ *  expression stack.  The shallower child (pushed later) must specify
+ *  a map bin.  The deeper child (pushed earlier) must contain a
+ *  logical subexpression containing one or more matching iteration
+ *  variable expressions.  The mapval iteration traverses the map and
+ *  repeatedly evaluates the subexpression substituting each map value
+ *  into the matching iteration variable.  The result of the iteration
+ *  expression is a logical OR of all of the individual element
+ *  evaluations.
  *
  *  For example, the following sequence of predicate expressions
  *  selects records where one of the map values is 0:
@@ -705,7 +874,17 @@ as_predexp_base*
 as_predexp_mapval_iterate_or(char const * varname);
 
 /**
- *	Create an map value iteration AND predicate expression.
+ *	Create an map value iteration AND logical predicate expression.
+ *
+ *  The mapval iteration expression pops two children off the
+ *  expression stack.  The shallower child (pushed later) must specify
+ *  a map bin.  The deeper child (pushed earlier) must contain a
+ *  logical subexpression containing one or more matching iteration
+ *  variable expressions.  The mapval iteration traverses the map and
+ *  repeatedly evaluates the subexpression substituting each map value
+ *  into the matching iteration variable.  The result of the iteration
+ *  expression is a logical AND of all of the individual element
+ *  evaluations.
  *
  *  For example, the following sequence of predicate expressions
  *  selects records where none of the map values is 0:

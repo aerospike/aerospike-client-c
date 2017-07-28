448a3672c1d30eae7f542548f03e2660180e4dec
Brian Nichols <bnichols@aerospike.com>
2017-07-28 14:15:04 -0700

    Cleanup doxygen comments.  Expand on Main Page comments.

--- src/include/aerospike/as_config.h
+++ src/include/aerospike/as_config.h
@@ -598,8 +598,10 @@ typedef struct as_config_s {
 	bool use_shm;
 
 	/**
-	 *	Shared memory identifier.  This identifier should be the same for all applications
-	 *	that use the Aerospike C client. 
+	 *	Identifier for the shared memory segment associated with the target Aerospike cluster.
+	 *	Each shared memory segment contains state for one Aerospike cluster.  If there are
+	 *	multiple Aerospike clusters, a different shm_key must be defined for each cluster.
+	 *	
 	 *	Default: 0xA6000000
 	 */
 	int shm_key;

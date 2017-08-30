db2556892e9e52b799856716f829df556c0e1032
Brian Nichols <bnichols@aerospike.com>
2017-08-30 09:43:02 -0700

    Initialize cmd->pos and cmd->len after TLS connect for libevent too.

--- src/main/aerospike/as_event_event.c
+++ src/main/aerospike/as_event_event.c
@@ -602,6 +602,7 @@ as_event_tls_connect(as_event_command* cmd, as_event_connection* conn)
 			cmd->state = AS_ASYNC_STATE_AUTH_WRITE;
 		}
 		else {
+			as_event_set_write(cmd);
 			cmd->state = AS_ASYNC_STATE_COMMAND_WRITE;
 		}
 		as_event_watch_write(cmd);

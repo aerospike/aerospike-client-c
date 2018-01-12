34676ec8cc3b05db8cbd3c1fff4cebaa642cd01b
Brian Nichols <bnichols@aerospike.com>
2018-01-11 16:02:54 -0800

    Update copyright year.

--- benchmarks/src/main/linear.c
+++ benchmarks/src/main/linear.c
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2008-2017 by Aerospike.
+ * Copyright 2008-2018 by Aerospike.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to

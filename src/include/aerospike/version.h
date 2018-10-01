dfa906a42ce87298b0d47a73138784755b224678
Brian Nichols <bnichols@aerospike.com>
2018-10-01 10:20:33 -0700

    Update version 4.3.18

--- src/include/aerospike/version.h
+++ src/include/aerospike/version.h
@@ -3,6 +3,6 @@
 // N: minor
 // P: patch
 // B: build id
-#define AEROSPIKE_CLIENT_VERSION 403170000L
+#define AEROSPIKE_CLIENT_VERSION 403180000L
 
 extern char* aerospike_client_version;
--- src/main/aerospike/version.c
+++ src/main/aerospike/version.c
@@ -1,1 +1,1 @@
-char* aerospike_client_version = "4.3.17";
+char* aerospike_client_version = "4.3.18";

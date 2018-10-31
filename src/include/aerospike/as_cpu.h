e11fb473979221d0851b75b698462468d44735a3
Brian Nichols <bnichols@aerospike.com>
2018-10-31 15:28:48 -0700

    Re-port to alpine linux.

--- src/include/aerospike/as_cpu.h
+++ src/include/aerospike/as_cpu.h
@@ -36,7 +36,7 @@ extern "C" {
 static inline int
 as_cpu_assign_thread_attr(pthread_attr_t* attr, int cpu_id)
 {
-#if defined(__APPLE__)
+#if defined(__APPLE__) || defined(AS_ALPINE)
 	// CPU affinity will be set later.
 	return 0;
 #else
@@ -57,6 +57,11 @@ as_cpu_assign_thread(pthread_t thread, int cpu_id)
 	thread_affinity_policy_data_t policy = {cpu_id};
 	thread_port_t mach_thread = pthread_mach_thread_np(thread);
 	return thread_policy_set(mach_thread, THREAD_AFFINITY_POLICY, (thread_policy_t)&policy, 1);
+#elif defined(AS_ALPINE)
+	cpu_set_t cpuset;
+	CPU_ZERO(&cpuset);
+	CPU_SET(cpu_id, &cpuset);
+	return pthread_setaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
 #else
 	// CPU affinity already set.
 	return 0;

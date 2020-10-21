5ab955fc75f2d5fe18fc08380424cd789d7dda74
Brian Nichols <bnichols@aerospike.com>
2020-10-21 13:30:57 -0700

    Update version 5.0.0

--- src/include/aerospike/version.h
+++ src/include/aerospike/version.h
@@ -3,6 +3,6 @@
 // N: minor
 // P: patch
 // B: build id
-#define AEROSPIKE_CLIENT_VERSION 406180000L
+#define AEROSPIKE_CLIENT_VERSION 500000000L
 
 extern char* aerospike_client_version;
--- src/main/aerospike/version.c
+++ src/main/aerospike/version.c
@@ -1,1 +1,1 @@
-char* aerospike_client_version = "4.6.18";
+char* aerospike_client_version = "5.0.0";

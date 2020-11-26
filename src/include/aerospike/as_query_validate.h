fdb718356ddef858cc39f475bd26f4b121e312da
Brian Nichols <bnichols@aerospike.com>
2020-11-26 14:27:49 -0800

    CLIENT-1380 Async queries timeout in the delay queue when fail_on_cluster_change is true and some query node commands are placed in delay queue. Fix by releasing query node command (which decrements event loop pending counter) before final fail_on_cluster_change validation.

--- src/include/aerospike/as_query_validate.h
+++ src/include/aerospike/as_query_validate.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008-2018 Aerospike, Inc.
+ * Copyright 2008-2020 Aerospike, Inc.
  *
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
  * license agreements.
@@ -29,6 +29,7 @@ extern "C" {
 struct as_node_s;
 struct as_event_executor;
 struct as_event_command;
+struct as_event_loop;
 
 /******************************************************************************
  * FUNCTIONS
@@ -57,7 +58,9 @@ as_query_validate(
  * Verify migrations are not occurring and obtain cluster key in async mode.
  */
 as_status
-as_query_validate_begin_async(struct as_event_executor* executor, const as_namespace ns, as_error* err);
+as_query_validate_begin_async(
+	struct as_event_executor* executor, const as_namespace ns, as_error* err
+	);
 
 /**
  * @private
@@ -73,7 +76,9 @@ as_query_validate_next_async(struct as_event_executor* executor, uint32_t index)
  * Then complete query.
  */
 void
-as_query_validate_end_async(struct as_event_command* cmd);
+as_query_validate_end_async(
+	struct as_event_executor* executor, struct as_node_s* node, struct as_event_loop* event_loop
+	);
 
 #ifdef __cplusplus
 } // end extern "C"

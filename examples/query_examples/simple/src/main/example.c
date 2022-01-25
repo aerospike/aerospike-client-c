410d0dc6c6dfca51528eb09727f8cfee95ca06be
Brian Nichols <bnichols@aerospike.com>
2022-01-25 11:44:37 -0800

    Add query partition examples.

--- examples/query_examples/simple/src/main/example.c
+++ examples/query_examples/simple/src/main/example.c
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2008-2018 by Aerospike.
+ * Copyright 2008-2022 by Aerospike.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
@@ -20,7 +20,6 @@
  * IN THE SOFTWARE.
  ******************************************************************************/
 
-
 //==========================================================
 // Includes
 //
@@ -31,8 +30,10 @@
 #include <aerospike/aerospike.h>
 #include <aerospike/aerospike_key.h>
 #include <aerospike/aerospike_query.h>
+#include <aerospike/as_cluster.h>
 #include <aerospike/as_error.h>
 #include <aerospike/as_key.h>
+#include <aerospike/as_partition.h>
 #include <aerospike/as_query.h>
 #include <aerospike/as_record.h>
 #include <aerospike/as_status.h>
@@ -40,13 +41,11 @@
 
 #include "example_utils.h"
 
-
 //==========================================================
 // Constants
 //
 
 const char TEST_INDEX_NAME[] = "test-bin-index";
-
 
 //==========================================================
 // Forward Declarations
@@ -55,10 +54,12 @@ const char TEST_INDEX_NAME[] = "test-bin-index";
 bool query_cb(const as_val* p_val, void* udata);
 void cleanup(aerospike* p_as);
 bool insert_records(aerospike* p_as);
-
+as_status query_partition(aerospike* p_as, as_error* err);
+as_status query_pages(aerospike* p_as, as_error* err);
+as_status query_terminate_resume(aerospike* p_as, as_error* err);
 
 //==========================================================
-// SIMPLE QUERY Example
+// SIMPLE QUERY Examples
 //
 
 int
@@ -122,11 +123,31 @@ main(int argc, char* argv[])
 
 	as_query_destroy(&query);
 
+	// Run query partition functionality.
+	if (query_partition(&as, &err) != AEROSPIKE_OK) {
+		LOG("query_partition() returned %d - %s", err.code, err.message);
+		cleanup(&as);
+		exit(-1);
+	}
+
+	// Run query pages.
+	if (query_pages(&as, &err) != AEROSPIKE_OK) {
+		LOG("query_pages() returned %d - %s", err.code, err.message);
+		cleanup(&as);
+		exit(-1);
+	}
+
+	// Run query terminate/resume.
+	if (query_terminate_resume(&as, &err) != AEROSPIKE_OK) {
+		LOG("query_terminate_resume() returned %d - %s", err.code, err.message);
+		cleanup(&as);
+		exit(-1);
+	}
+
 	// Cleanup and disconnect from the database cluster.
 	cleanup(&as);
 
 	LOG("simple query example successfully completed");
-
 	return 0;
 }
 
@@ -203,4 +224,320 @@ insert_records(aerospike* p_as)
 	LOG("insert succeeded");
 
 	return true;
+}
+
+//==========================================================
+// Query Partition
+//
+
+static const char* g_pset = "pqset";
+
+static as_status
+insert_records_in_one_partition(aerospike* p_as, as_error* err, uint32_t part_id, uint32_t* rec_count)
+{
+	// Write records that belong to the specified partition.
+	as_record rec;
+	as_record_inita(&rec, 1);
+	as_record_set_int64(&rec, "bin1", 55);
+
+	uint32_t count = 0;
+	as_status status;
+
+	for (uint32_t i = 0; i < 80000; i++) {
+		as_key key;
+		as_key_init_int64(&key, g_namespace, g_pset, (int64_t)i);
+
+		status = as_key_set_digest(err, &key);
+
+		if (status != AEROSPIKE_OK) {
+			return status;
+		}
+
+		uint32_t id = as_partition_getid(key.digest.value, p_as->cluster->n_partitions);
+
+		if (id != part_id) {
+			continue;
+		}
+
+		status = aerospike_key_put(p_as, err, NULL, &key, &rec);
+
+		if (status != AEROSPIKE_OK) {
+			return status;
+		}
+		count++;
+	}
+	*rec_count = count;
+	return AEROSPIKE_OK;
+}
+
+struct counter {
+	uint32_t count;
+	uint32_t max;
+	as_digest digest;
+};
+
+static bool
+pquery_cb1(const as_val* val, void* udata)
+{
+	if (! val) {
+		// Query complete.
+		return true;
+	}
+
+	struct counter* c = udata;
+
+	if (as_aaf_uint32(&c->count, 1) == c->max) {
+		// Save digest cursor and stop query.
+		as_record* rec = as_record_fromval(val);
+		c->digest = rec->key.digest;
+		return false;
+	}
+	return true;
+}
+
+static bool
+pquery_cb2(const as_val* val, void* udata)
+{
+	if (! val) {
+		// Query complete.
+		return true;
+	}
+
+	struct counter* c = udata;
+	as_incr_uint32(&c->count);
+	return true;
+}
+
+as_status
+query_partition(aerospike* p_as, as_error* err)
+{
+	LOG("write records for partition query");
+
+	// Write records that belong to a single partition.
+	uint32_t part_id = 1000;
+	uint32_t rec_count;
+	as_status status = insert_records_in_one_partition(p_as, err, part_id, &rec_count);
+
+	if (status != AEROSPIKE_OK) {
+		return status;
+	}
+
+	LOG("records written: %u", rec_count);
+	LOG("query partition");
+
+	// Read first half of records from that partition.
+	struct counter c;
+	c.count = 0;
+	c.max = rec_count / 2;
+	c.digest.init = false;
+
+	as_query query;
+	as_query_init(&query, g_namespace, g_pset);
+
+	as_partition_filter pf;
+	as_partition_filter_set_id(&pf, part_id);
+
+	status = aerospike_query_partitions(p_as, err, NULL, &query, &pf, pquery_cb1, &c);
+
+	if (status != AEROSPIKE_OK) {
+		as_query_destroy(&query);
+		return status;
+	}
+
+	LOG("records returned: %u", c.count);
+	LOG("query partition again from cursor");
+
+	// Read remaining records from that partition using digest cursor.
+	as_partition_filter_set_after(&pf, &c.digest);
+	c.count = 0;
+
+	status = aerospike_query_partitions(p_as, err, NULL, &query, &pf, pquery_cb2, &c);
+
+	if (status != AEROSPIKE_OK) {
+		as_query_destroy(&query);
+		return status;
+	}
+
+	LOG("records returned: %u", c.count);
+	as_query_destroy(&query);
+	return AEROSPIKE_OK;
+}
+
+//==========================================================
+// Query Pages
+//
+
+static as_status
+insert_records_for_query_page(aerospike* p_as, as_error* err, const as_set set, uint32_t size)
+{
+	// Write records that belong to the specified partition.
+	as_record rec;
+	as_record_inita(&rec, 1);
+	as_record_set_int64(&rec, "bin1", 55);
+
+	as_status status;
+
+	for (uint32_t i = 0; i < size; i++) {
+		as_key key;
+		as_key_init_int64(&key, g_namespace, set, (int64_t)i);
+
+		status = as_key_set_digest(err, &key);
+
+		if (status != AEROSPIKE_OK) {
+			return status;
+		}
+
+		status = aerospike_key_put(p_as, err, NULL, &key, &rec);
+
+		if (status != AEROSPIKE_OK) {
+			return status;
+		}
+	}
+	return AEROSPIKE_OK;
+}
+
+as_status
+query_pages(aerospike* p_as, as_error* err)
+{
+	const char* set = "querypage";
+	uint32_t total_size = 190;
+	uint32_t page_size = 100;
+
+	LOG("write records for query pagination");
+	as_status status = insert_records_for_query_page(p_as, err, set, total_size);
+
+	if (status != AEROSPIKE_OK) {
+		return status;
+	}
+
+	LOG("records written: %u", total_size);
+
+	struct counter c;
+	c.count = 0;
+
+	as_query query;
+	as_query_init(&query, g_namespace, set);
+	as_query_set_paginate(&query, true);
+	query.max_records = page_size;
+
+	// Query 3 pages of records.
+	for (int i = 1; i <= 3 && ! as_query_is_done(&query); i++) {
+		c.count = 0;
+
+		LOG("query page: %d", i);
+		status = aerospike_query_foreach(p_as, err, NULL, &query, pquery_cb2, &c);
+
+		if (status != AEROSPIKE_OK) {
+			as_query_destroy(&query);
+			return status;
+		}
+		LOG("records returned: %u", c.count);
+	}
+
+	as_query_destroy(&query);
+	return AEROSPIKE_OK;
+}
+
+//==========================================================
+// Query Terminate and Resume
+//
+
+static bool
+query_terminate_cb(const as_val* val, void* udata)
+{
+	if (! val) {
+		// Query complete.
+		return true;
+	}
+
+	struct counter* c = udata;
+
+	// query.concurrent is false, so atomics are not necessary.
+	if (c->count >= c->max) {
+		// Since we are terminating the query here, the query last digest
+		// will not be set and the current record will be returned again
+		// if the query resumes at a later time.
+		return false;
+	}
+
+	c->count++;
+	return true;
+}
+
+static bool
+query_resume_cb(const as_val* val, void* udata)
+{
+	if (! val) {
+		// Query complete.
+		return true;
+	}
+
+	struct counter* c = udata;
+	c->count++;
+	return true;
+}
+
+
+as_status
+query_terminate_resume(aerospike* p_as, as_error* err)
+{
+	const char* set = "queryresume";
+	uint32_t total_size = 200;
+
+	LOG("write records for query terminate/resume");
+	as_status status = insert_records_for_query_page(p_as, err, set, total_size);
+
+	if (status != AEROSPIKE_OK) {
+		return status;
+	}
+
+	LOG("records written: %u", total_size);
+	LOG("start query terminate");
+
+	struct counter c;
+	c.count = 0;
+	c.max = 50;
+
+	as_query query;
+	as_query_init(&query, g_namespace, set);
+	as_query_set_paginate(&query, true);
+
+	// Start query. Query will be terminated early in callback.
+	status = aerospike_query_foreach(p_as, err, NULL, &query, query_terminate_cb, &c);
+
+	if (status != AEROSPIKE_OK) {
+		as_query_destroy(&query);
+		return status;
+	}
+
+	LOG("terminate records returned: %u", c.count);
+	LOG("start query resume");
+
+	// Store completion status of all partitions.
+	as_partitions_status* parts_all = as_partitions_status_reserve(query.parts_all);
+
+	// Destroy query
+	as_query_destroy(&query);
+
+	// Resume query using new query instance.
+	as_query query_resume;
+	as_query_init(&query_resume, g_namespace, set);
+
+	// Use partition filter to set parts_all.
+	// Calling as_query_set_partitions(&query_resume, parts_all) works too.
+	// as_partition_filter_set_partitions() is just a wrapper for eventually calling
+	// as_query_set_partitions().
+	as_partition_filter pf;
+	as_partition_filter_set_partitions(&pf, parts_all);
+
+	c.count = 0;
+	c.max = 0;
+
+	status = aerospike_query_partitions(p_as, err, NULL, &query_resume, &pf, query_resume_cb, &c);
+
+	LOG("resume records returned: %u", c.count);
+
+	as_partitions_status_release(parts_all);
+	as_query_destroy(&query_resume);
+	return status;
 }

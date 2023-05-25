bf17925dbe9a6e57a6a092d0f889bde2ca748282
Brian Nichols <bnichols@aerospike.com>
2023-05-25 10:38:19 -0700

    Workaround for server returning partition unavailable error after accepting connections.

--- src/main/aerospike/as_partition_tracker.c
+++ src/main/aerospike/as_partition_tracker.c
@@ -537,6 +537,7 @@ as_partition_tracker_should_retry(
 	)
 {
 	switch (status) {
+	case AEROSPIKE_ERR_CLUSTER:
 	case AEROSPIKE_ERR_CONNECTION:
 	case AEROSPIKE_ERR_ASYNC_CONNECTION:
 	case AEROSPIKE_ERR_TIMEOUT:

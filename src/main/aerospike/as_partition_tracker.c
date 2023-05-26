85b8f5942443d2231c4289b2b4e59d39266a549c
Brian Nichols <bnichols@aerospike.com>
2023-05-26 10:43:38 -0700

    Add AEROSPIKE_ERR_INVALID_NODE to list of retryable query errors.

--- src/main/aerospike/as_partition_tracker.c
+++ src/main/aerospike/as_partition_tracker.c
@@ -541,6 +541,7 @@ as_partition_tracker_should_retry(
 	case AEROSPIKE_ERR_CONNECTION:
 	case AEROSPIKE_ERR_ASYNC_CONNECTION:
 	case AEROSPIKE_ERR_TIMEOUT:
+	case AEROSPIKE_ERR_INVALID_NODE:
 	case AEROSPIKE_ERR_INDEX_NOT_FOUND:
 	case AEROSPIKE_ERR_INDEX_NOT_READABLE:
 		// Multiple scan/query threads may call this function, so error

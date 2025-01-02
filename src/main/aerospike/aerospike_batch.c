b1073c24354c9f208001dd43100f34ebeaa555b1
Brian Nichols <bnichols@aerospike.com>
2025-01-02 14:28:39 -0500

    Update copyright year.

--- src/main/aerospike/aerospike_batch.c
+++ src/main/aerospike/aerospike_batch.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008-2024 Aerospike, Inc.
+ * Copyright 2008-2025 Aerospike, Inc.
  *
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
  * license agreements.

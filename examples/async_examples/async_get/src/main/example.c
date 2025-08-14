c2f4b2e51315815675e98a7507d38baf23a7422d
Brian Nichols <bnichols@aerospike.com>
2025-08-14 16:36:25 -0400

    Change async_get example to pass in a policy.
    Remove -DAS_USE_LIBEV from xcode.

--- examples/async_examples/async_get/src/main/example.c
+++ examples/async_examples/async_get/src/main/example.c
@@ -74,9 +74,14 @@ main(int argc, char* argv[])
 	// Start clean.
 	example_remove_test_record(&as);
 
+	as_policy_read p;
+	as_policy_read_init(&p);
+	p.base.total_timeout = 500;
+	p.base.timeout_delay = 3000;
+
 	// Try to read the test record from the database. This should fail since the record is not there.
 	as_error err;
-	if (aerospike_key_get_async(&as, &err, NULL, &g_key, expect_not_found, NULL, NULL, NULL) != AEROSPIKE_OK) {
+	if (aerospike_key_get_async(&as, &err, &p, &g_key, expect_not_found, NULL, NULL, NULL) != AEROSPIKE_OK) {
 		LOG("aerospike_key_get_async() returned %d - %s", err.code, err.message);
 		example_cleanup(&as);
 		as_event_close_loops();

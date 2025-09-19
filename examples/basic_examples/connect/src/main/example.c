e1cb8bda5879e2eb9abf6bc5e6df4a0f40e8cf01
Samuel A. Falvo II <sfalvo@aerospike.com>
2025-09-19 11:38:57 -0700

    CLIENT-3532  Include copyright notice.
    This patch also includes comment banners for different sections of code,
    inspired by looking at other code examples in the basic_examples directory.

--- examples/basic_examples/connect/src/main/example.c
+++ examples/basic_examples/connect/src/main/example.c
@@ -1,3 +1,30 @@
+/*******************************************************************************
+ * Copyright 2008-2025 by Aerospike.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ ******************************************************************************/
+
+
+//========================================================================
+// Includes
+//
+
 #include <errno.h>
 #include <getopt.h>
 #include <stdbool.h>
@@ -7,6 +34,11 @@
 #include <unistd.h>
 
 #include <aerospike/aerospike.h>
+
+
+//========================================================================
+// Command line option processing
+//
 
 // The result of parsing arguments from the CLI.
 typedef struct program_options_s {
@@ -124,6 +156,11 @@ program_options_parse(program_options* po, int argc, char* argv[])
 	// are given.
 	po->tls_options_given = po->cafile && po->tls_name;
 }
+
+
+//========================================================================
+// CONNECT Example
+//
 
 void
 check_error(const char* operation, as_error* err)

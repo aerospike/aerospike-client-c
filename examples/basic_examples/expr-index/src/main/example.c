d4643efcda592498a214c7f33b6a11c3bf8f010a
Samuel A. Falvo II <sfalvo@aerospike.com>
2025-10-16 17:43:48 -0700

    CLIENT-3772: Print Base64 repr of index expression

---
+++ examples/basic_examples/expr-index/src/main/example.c
@@ -0,0 +1,221 @@
+/*******************************************************************************
+ * Copyright 2008-2025 by Aerospike.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ ******************************************************************************/
+
+
+//==========================================================
+// Includes
+//
+
+#include <stddef.h>
+#include <stdlib.h>
+
+#include <aerospike/aerospike.h>
+#include <aerospike/as_exp.h>
+
+// #include <aerospike/aerospike_key.h>
+// #include <aerospike/as_error.h>
+// #include <aerospike/as_record.h>
+// #include <aerospike/as_sleep.h>
+// #include <aerospike/as_status.h>
+
+#include <aerospike/as_arraylist.h>
+
+#include "example_utils.h"
+
+
+//==========================================================
+// Forward Declarations
+//
+
+static int do_create_expression(void);
+
+
+//==========================================================
+// GET Example
+//
+
+int
+main(int argc, char* argv[])
+{
+	// Parse command line arguments.
+	if (! example_get_opts(argc, argv, EXAMPLE_BASIC_OPTS_SUBCMD)) {
+		exit(-1);
+	}
+
+	if (! strcmp(g_subcommand, "create-expression")) {
+		return do_create_expression();
+	}
+
+	LOG("Unrecognized sub-command: %s", g_subcommand);
+	LOG("Available sub-commands include:");
+	LOG("  create-expression -- creates expression and prints base-64 code");
+
+	return 1;
+}
+
+
+#if 0
+	// Connect to the aerospike database cluster.
+	aerospike as;
+	example_connect_to_aerospike(&as);
+
+	// Start clean.
+	example_remove_test_record(&as);
+
+	as_error err;
+	as_record* p_rec = NULL;
+
+	// Try to read the test record from the database. This should fail since the
+	// record is not there.
+	if (aerospike_key_get(&as, &err, NULL, &g_key, &p_rec) !=
+			AEROSPIKE_ERR_RECORD_NOT_FOUND) {
+		LOG("aerospike_key_get() returned %d - %s, expected "
+				"AEROSPIKE_ERR_RECORD_NOT_FOUND", err.code, err.message);
+		as_record_destroy(p_rec);
+		example_cleanup(&as);
+		exit(-1);
+	}
+
+	// Note that p_rec will still be NULL here.
+	LOG("get (non-existent record) failed as expected");
+
+	// Write a record to the database so we can demonstrate read success.
+	if (! write_record(&as)) {
+		example_cleanup(&as);
+		exit(-1);
+	}
+
+	// Read the (whole) test record from the database.
+	if (aerospike_key_get(&as, &err, NULL, &g_key, &p_rec) != AEROSPIKE_OK) {
+		LOG("aerospike_key_get() returned %d - %s", err.code, err.message);
+		example_cleanup(&as);
+		exit(-1);
+	}
+
+	// Log the result and recycle the as_record object.
+	LOG("record was successfully read from database:");
+	example_dump_record(p_rec);
+	as_record_destroy(p_rec);
+	p_rec = NULL;
+
+	// Select bins 1 and 3 to read.
+	static const char* bins_1_3[] = { "test-bin-1", "test-bin-3", NULL };
+
+	// Read only these two bins of the test record from the database.
+	if (aerospike_key_select(&as, &err, NULL, &g_key, bins_1_3, &p_rec) !=
+			AEROSPIKE_OK) {
+		LOG("aerospike_key_select() returned %d - %s", err.code, err.message);
+		example_cleanup(&as);
+		exit(-1);
+	}
+
+	// Log the result and recycle the as_record object.
+	LOG("bins 1 and 3 were read from database:");
+	example_dump_record(p_rec);
+	as_record_destroy(p_rec);
+	p_rec = NULL;
+
+	// Select non-existent bin 5 to read.
+	static const char* bins_5[] = { "test-bin-5", NULL };
+
+	// Read only this bin from the database. This call should return an
+	// as_record object with one bin, with null as_bin_value.
+	if (aerospike_key_select(&as, &err, NULL, &g_key, bins_5, &p_rec) !=
+			AEROSPIKE_OK) {
+		LOG("aerospike_key_select() returned %d - %s", err.code, err.message);
+		example_cleanup(&as);
+		exit(-1);
+	}
+
+	// Log the result and destroy the as_record object.
+	LOG("non-existent bin 5 was read from database:");
+	example_dump_record(p_rec);
+	as_record_destroy(p_rec);
+	p_rec = NULL;
+
+	// Sleep 2 seconds, just to show the TTL decrease.
+	LOG("waiting 2 seconds ...");
+	as_sleep(2000);
+
+	// Use aerospike_key_exists() to get only record metadata.
+	if (aerospike_key_exists(&as, &err, NULL, &g_key, &p_rec) != AEROSPIKE_OK) {
+		LOG("aerospike_key_exists() returned %d - %s", err.code, err.message);
+		example_cleanup(&as);
+		exit(-1);
+	}
+
+	// Log the result, which will only have metadata.
+	LOG("existence check found record metadata:");
+	example_dump_record(p_rec);
+	as_record_destroy(p_rec);
+
+	// Cleanup and disconnect from the database cluster.
+	example_cleanup(&as);
+
+	LOG("get example successfully completed");
+
+	return 0;
+}
+#endif
+
+
+//==========================================================
+// Helpers
+//
+
+static int
+do_create_expression(void)
+{
+	as_arraylist countries_of_interest;
+	as_arraylist_init(&countries_of_interest, 3, 3);
+	as_arraylist_append_str(&countries_of_interest, "Australia");
+	as_arraylist_append_str(&countries_of_interest, "Canada");
+	as_arraylist_append_str(&countries_of_interest, "Botswana");
+
+	as_exp_build(filter_exp,
+		as_exp_cond(
+			as_exp_and(
+				as_exp_cmp_ge(
+					as_exp_bin_int("age"),
+					as_exp_int(18)),
+				as_exp_list_get_by_value_list(
+					NULL, AS_LIST_RETURN_EXISTS,
+					as_exp_bin_list("country"),
+					as_exp_val(&countries_of_interest))),
+
+			// If true, return the value in the age bin
+			as_exp_bin_int("age"),
+
+			// return unknown to exclude value from index
+			as_exp_unknown()
+		));
+
+	LOG("Expression base64 = %s", as_exp_to_base64(filter_exp));
+	return 0;
+}
+
+// lHuTEJMEk1ECo2FnZRKVfwEAkxYNk1EDp2NvdW50cnmSfpOqA0F1c3RyYWxpYacDQ2FuYWRhqQNCb3Rzd2FuYZNRAqNhZ2WRAA Java
+// lHuTEJMEk1ECo2FnZRKVfwEAkxYNk1EDp2NvdW50cnmSfpOqA0F1c3RyYWxpYacDQ2FuYWRhqQNCb3Rzd2FuYZNRAqNhZ2WRAA Python
+// lHuTEJMEk1ECo2FnZRKVfwEAkxYNk1EDp2NvdW50cnmSfpOqA0F1c3RyYWxpYacDQ2FuYWRhqQNCb3Rzd2FuYZNRAqNhZ2WRAA C#
+// lHuTEJMEk1ECo2FnZRKVfwEAkxcNk1EEp2NvdW50cnmSfpOqA0F1c3RyYWxpYacDQ2FuYWRhqQNCb3Rzd2FuYZNRAqNhZ2WRAA C
+//                                ^-- ONE character difference.  TODO: Is this significant?
+

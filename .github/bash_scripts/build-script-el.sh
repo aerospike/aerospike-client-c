5ec58c9e25db00846748ec2ffe7b529bf8c348b8
Samuel A. Falvo II <sfalvo@aerospike.com>
2026-05-14 15:58:23 -0700

    CLIENT-4299: refactor scripts

--- .github/bash_scripts/build-script-el.sh
+++ .github/bash_scripts/build-script-el.sh
@@ -32,15 +32,5 @@ podman run \
 	--workdir /workspace \
 	--userns=keep-id \
 	$PODMANIMG \
-	/bin/bash -c "
-yum install -y openssl-devel glibc-devel autoconf automake libtool libz-devel libyaml-devel gcc-c++ graphviz rpm-build
-
-./install_libuv
-./install_libev
-./install_libevent
-make clean
-make
-sudo make install
-make package
-"
+	/bin/bash -c "./.github/bash_scripts/container_build_script_el.sh"
 
---
+++ .github/bash_scripts/container-build-script-el.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -ue
+
+yum install -y openssl-devel glibc-devel autoconf automake libtool libz-devel libyaml-devel gcc-c++ graphviz rpm-build
+
+./install_libuv
+./install_libev
+./install_libevent
+make clean
+make
+sudo make install
+make package

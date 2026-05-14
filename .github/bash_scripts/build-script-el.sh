918292b3d18c6ec769f6a97ec5d73d3e57ceba0b
Samuel A. Falvo II <sfalvo@aerospike.com>
2026-05-14 16:45:46 -0700

    CLIENT-4299: I see dead processes...
    ... and they don't even know they're dead.
    
    Probably because the distinction between what is and isn't a file is not
    obvious from the choice of environment variables used.  All because jq is
    hallucinating a control character that I'm positive does not exist.

--- .github/bash_scripts/build-script-el.sh
+++ .github/bash_scripts/build-script-el.sh
@@ -10,7 +10,7 @@
 set -ue
 
 echo --------------------------------------------------------------------------------
-xxd $MATRIX_JSON
+xxd <<< \"$MATRIX_JSON\"
 echo --------------------------------------------------------------------------------
 export DISTRO=$(jq -r '.distro' <<<\"$MATRIX_JSON\")
 export ARCH=$(jq -r '.arch' <<<\"$MATRIX_JSON\")
